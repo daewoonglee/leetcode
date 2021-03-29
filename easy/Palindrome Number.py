a08c65a37d995b46be860f585b85719f4edc7fb0
daewoong lee <dw.lee832@gmail.com>
2021-03-29 23:44:13 +0900

    Palindrome Number 문제풀이 01 - 풀이O (using str)

---
+++ easy/Palindrome Number.py
@@ -0,0 +1,54 @@
+"""
+Given an integer x, return true if x is palindrome integer.
+An integer is a palindrome when it reads the same backward as forward.
+For example, 121 is palindrome while 123 is not.
+
+
+Example 1:
+Input: x = 121
+Output: true
+
+Example 2:
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-.
+Therefore it is not a palindrome.
+
+Example 3:
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+Example 4:
+Input: x = -101
+Output: false
+
+Constraints:
+-2^31 <= x <= 2^31 - 1
+
+Follow up: Could you solve it without converting the integer to a string?
+"""
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x)
+        N = len(x)
+        if N % 2 == 0:
+            return True if x[:N//2] == x[N//2:][::-1] else False
+        else:
+            return True if x[:N//2] == x[N//2+1:][::-1] else False
+
+
+s = Solution()
+# print(s.isPalindrome(121))
+# print(s.isPalindrome(-121))
+# print(s.isPalindrome(10))
+# print(s.isPalindrome(-101))
+print(s.isPalindrome(1001))
+print(s.isPalindrome(10001))
+print(s.isPalindrome(1000021))
