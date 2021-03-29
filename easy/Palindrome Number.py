643e28fddd415472f8040f5d674a0230d5e83f8e
daewoong lee <dw.lee832@gmail.com>
2021-03-29 23:56:17 +0900

    Palindrome Number 문제풀이 01 - without using str

--- easy/Palindrome Number.py
+++ easy/Palindrome Number.py
@@ -30,25 +30,39 @@ Follow up: Could you solve it without converting the integer to a string?
 """
 
 
+import math
 class Solution(object):
     def isPalindrome(self, x):
         """
         :type x: int
         :rtype: bool
         """
-        x = str(x)
-        N = len(x)
-        if N % 2 == 0:
-            return True if x[:N//2] == x[N//2:][::-1] else False
-        else:
-            return True if x[:N//2] == x[N//2+1:][::-1] else False
+        # using str version
+        # x = str(x)
+        # N = len(x)
+        # if N % 2 == 0:
+        #     return True if x[:N//2] == x[N//2:][::-1] else False
+        # else:
+        #     return True if x[:N//2] == x[N//2+1:][::-1] else False
 
+        # without converting int to str
+        if x < 0:
+            return False
+        li = []
+        while x != 0:
+            x, r = divmod(x, 10)
+            li.append(r)
+        N = len(li)
+        if N % 2 == 0:
+            return True if li[:N//2] == li[N//2:][::-1] else False
+        else:
+            return True if li[:N//2] == li[N//2+1:][::-1] else False
 
 s = Solution()
-# print(s.isPalindrome(121))
-# print(s.isPalindrome(-121))
-# print(s.isPalindrome(10))
-# print(s.isPalindrome(-101))
+print(s.isPalindrome(121))
+print(s.isPalindrome(-121))
+print(s.isPalindrome(10))
+print(s.isPalindrome(-101))
 print(s.isPalindrome(1001))
 print(s.isPalindrome(10001))
 print(s.isPalindrome(1000021))
