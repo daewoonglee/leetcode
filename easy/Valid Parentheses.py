d98e871aa756263d66e928783991db8ae15eaba7
daewoong lee <dw.lee832@gmail.com>
2021-04-01 22:03:34 +0900

    Valid Parentheses 문제풀이 01 - code refactoring

--- easy/Valid Parentheses.py
+++ easy/Valid Parentheses.py
@@ -39,17 +39,30 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        table = {"(": 0, ")": 1, "[": 2, "]": 3, "{": 4, "}": 5}
-        stack = [s[0]]
-        idx = 1
-        N = len(s)
-        while idx < N:
-            if stack and table[stack[-1]]+1 == table[s[idx]]:
+        # 0.005914865857142857
+        # table = {"(": 0, ")": 1, "[": 2, "]": 3, "{": 4, "}": 5}
+        # stack = [s[0]]
+        # idx = 1
+        # N = len(s)
+        # while idx < N:
+        #     if stack and table[stack[-1]]+1 == table[s[idx]]:
+        #         stack.pop()
+        #     else:
+        #         stack.append(s[idx])
+        #     idx += 1
+        # return False if stack else True
+
+        # code refactoring - 0.004202033571428572
+        table = {"(": ")", "[": "]", "{": "}"}
+        stack = []
+        for char in s:
+            if char in table:
+                stack.append(char)
+            elif stack and table[stack[-1]] == char:
                 stack.pop()
             else:
-                stack.append(s[idx])
-            idx += 1
-        return False if stack else True
+                return False
+        return not stack
 
 
 s = Solution()
@@ -60,3 +73,17 @@ print(s.isValid("([)]"))
 print(s.isValid("{[]}"))
 print(s.isValid("(("))
 print(s.isValid(")("))
+
+
+import timeit
+avg_time = 0.
+tests = [["()"],
+         ["()[]{}"],
+         ["(]"],
+         ["([)]"],
+         ["{[]}"],
+         ["(("],
+         [")("]]
+for t in tests:
+    avg_time += timeit.timeit(lambda: s.isValid(t), number=10000)
+print(f'avg_time: {avg_time / len(tests)}')
