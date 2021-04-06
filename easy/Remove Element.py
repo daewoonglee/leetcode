92da65ba44f198a6fba56a5f7bcd6cc191a774b7
daewoong lee <dw.lee832@gmail.com>
2021-04-06 22:03:44 +0900

    Remove Element 문제풀이 01 - 풀이O

---
+++ easy/Remove Element.py
@@ -0,0 +1,84 @@
+"""
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Clarification:
+Confused why the returned value is an integer but your answer is an array?
+Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller as well.
+
+Internally you can think of this:
+// nums is passed in by reference. (i.e., without making a copy)
+int len = removeElement(nums, val);
+
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+
+
+Example 1:
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2]
+Explanation: Your function should return length = 2, with the first two elements of nums being 2.
+It doesn't matter what you leave beyond the returned length. For example if you return 2 with nums = [2,2,3,3] or nums = [2,2,0,0], your answer will be accepted.
+
+Example 2:
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3]
+Explanation: Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4. Note that the order of those five elements can be arbitrary.
+It doesn't matter what values are set beyond the returned length.
+
+Constraints:
+0 <= nums.length <= 100
+0 <= nums[i] <= 50
+0 <= val <= 100
+"""
+
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        # Solution 01 - pop
+        # running time: 3.596929037
+        # for i in range(len(nums)-1, -1, -1):
+        #     if nums[i] == val:
+        #         nums.pop(i)
+        # return len(nums)
+
+        # Solution 02 - assign
+        # running time: 4.615130591000001
+        idx = -1
+        for i in range(len(nums)):
+            if val == nums[i]:
+                idx = i
+                break
+        if idx == -1:
+            return len(nums)
+
+        cur_idx = idx
+        for i in range(idx+1, len(nums)):
+            if nums[i] != val:
+                nums[cur_idx] = nums[i]
+                cur_idx += 1
+        del nums[cur_idx:]
+        return len(nums)
+
+
+s = Solution()
+print(s.removeElement([3, 2, 2, 3], 3))
+print(s.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
+print(s.removeElement([0, 1], 2))
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    t = Timer("for t in [[[3, 2, 2, 3], 3], "
+              "[[0, 1, 2, 2, 3, 0, 4, 2], 2], "
+              "[[0, 1], 2], [[], 0]]: Solution().removeElement(*t)", "from __main__ import Solution")
+    print(t.timeit())
