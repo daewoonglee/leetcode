7c5aa607dd273913e3209a1b9055b51b21d42a40
daewoong lee <dw.lee832@gmail.com>
2021-04-07 22:17:29 +0900

    Implement strStr 문제풀이 01 - code refactoring

--- easy/Implement strStr.py
+++ easy/Implement strStr.py
@@ -8,11 +8,11 @@ For the purpose of this problem, we will return 0 when needle is an empty string
 This is consistent to C's strstr() and Java's indexOf().
 
 Example 1:
-Input: haystack = "hello", needle = "ll"
+Input: haystack = 'hello', needle = 'll'
 Output: 2
 
 Example 2:
-Input: haystack = "aaaaa", needle = "bba"
+Input: haystack = 'aaaaa', needle = 'bba'
 Output: -1
 
 Example 3:
@@ -32,17 +32,33 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        N = len(needle)
-        for i in range(len(haystack)-N+1):
-            if haystack[i: i+N] == needle:
-                return i
-        return -1
+        # 3.371248084
+        # N = len(needle)
+        # for i in range(len(haystack)-N+1):
+        #     if haystack[i: i+N] == needle:
+        #         return i
+        # return -1
+
+        # code refactoring
+        # 1.711348691
+        if not needle:
+            return 0
+        try:
+            index = haystack.index(needle)
+        except:
+            index = -1
+        return index
 
 
 s = Solution()
-print(s.strStr("hello", "ll"))
-print(s.strStr("aaaaa", "baa"))
-print(s.strStr("", ""))
-print(s.strStr("aabaa", "aa"))
+print(s.strStr('hello', 'll'))
+print(s.strStr('aaaaa', 'baa'))
+print(s.strStr('', ''))
+print(s.strStr('aabaa', 'aa'))
 
 
+if __name__ == '__main__':
+    from timeit import Timer
+    t = Timer("for t in [['hello', 'll'], ['aaaaa', 'baa'], ['', ''], ['aabaa', 'aa']]: Solution().strStr(*t)",
+              "from __main__ import Solution")
+    print(t.timeit())
