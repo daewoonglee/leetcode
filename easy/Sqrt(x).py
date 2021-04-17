6d9617e89733c6e9ef035f466f5550a4c71bec5c
daewoong lee <dw.lee832@gmail.com>
2021-04-17 14:13:00 +0900

    Sqrt(x) 문제풀이 01 - code refactoring

--- easy/Sqrt(x).py
+++ easy/Sqrt(x).py
@@ -23,10 +23,42 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        return int(math.sqrt(x))
+        # 0.03201561
+        # return int(math.sqrt(x))
+
+        # Implement integer sqrt method 01 - 0.103219589
+        # p = 1
+        # while p**2 <= x:
+        #     p += 1
+        # return p-1
+
+        # Newton–Raphson method - 0.11574681299999999
+        # pre_x, cur_x = 0, 5
+        # while 1:
+        #     cur_x = cur_x - ((cur_x**2)-x) / (2*cur_x)
+        #     if int(cur_x) != pre_x:
+        #         pre_x = int(cur_x)
+        #     else:
+        #         break
+        # return pre_x
+
+        # code refactoring 01 - 0.029433162000000002
+        return int(x**0.5)
 
 
 s = Solution()
-print(s.mySqrt(4))
-print(s.mySqrt(8))
+# print(s.mySqrt(0))
+# print(s.mySqrt(1))
+# print(s.mySqrt(2))
+# print(s.mySqrt(3))
+# print(s.mySqrt(4))
+# print(s.mySqrt(8))
 print(s.mySqrt(147))
+# print(s.mySqrt(1470))
+
+
+# if __name__ == '__main__':
+#     from timeit import Timer
+#     query = [0, 1, 2, 3, 4, 8, 147]
+#     t = Timer(f"for t in {query}: Solution().mySqrt(t)", "from __main__ import Solution")
+#     print(t.timeit(number=10000))
