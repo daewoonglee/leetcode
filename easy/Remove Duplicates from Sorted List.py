36f1e7e22b01448917686a465d948053ac3576c0
daewoong lee <dw.lee832@gmail.com>
2021-04-19 23:01:13 +0900

    Remove Duplicates from Sorted List 문제풀이 01 - code refactoring

--- easy/Remove Duplicates from Sorted List.py
+++ easy/Remove Duplicates from Sorted List.py
@@ -31,6 +31,7 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
+        # 0.018867328999999995
         if not head:
             return None
 
@@ -47,26 +48,49 @@ class Solution(object):
         #     print(node_head.val)
         #     node_head = node_head.next
 
-        ans_head = ListNode(node_head.val)
-        ans = ans_head
-        while node_head:
-            print(f"ans: {ans.val}, node: {node_head.val}")
-            if ans.val != node_head.val:
-                ans.next = node_head
+        # ans_head = ListNode(node_head.val)
+        # ans = ans_head
+        # while node_head:
+        #     # print(f"ans: {ans.val}, node: {node_head.val}")
+        #     if ans.val != node_head.val:
+        #         ans.next = node_head
+        #         ans = ans.next
+        #     node_head = node_head.next
+        # ans.next = None
+        #
+        # # print ListNode
+        # while ans_head:
+        #     # print(f"p: {ans_head.val}")
+        #     ans_head = ans_head.next
+        # return ans_head
+
+        # code refactoring - 0.017514743
+        ans = node_head
+        head = ans
+        while ans and ans.next:
+            if ans.val != ans.next.val:
                 ans = ans.next
-            node_head = node_head.next
-        ans.next = None
+            else:
+                ans.next = ans.next.next
 
         # print ListNode
-        while ans_head:
-            print(f"p: {ans_head.val}")
-            ans_head = ans_head.next
-        return ans_head
+        while head:
+            # print(f"p: {head.val}")
+            head = head.next
+        return head
 
 
 s = Solution()
-# print(s.deleteDuplicates([1, 1, 2]))
+print(s.deleteDuplicates([1, 1, 2]))
 # print(s.deleteDuplicates([1, 1, 2, 3, 3]))
 # print(s.deleteDuplicates([1, 1, 2, 3, 3, 4]))
 # print(s.deleteDuplicates([]))
-print(s.deleteDuplicates([0, 0, 0]))
+# print(s.deleteDuplicates([0, 0, 0]))
+# print(s.deleteDuplicates([1, 2, 2, 2]))
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [[1, 1, 2], [1, 1, 2, 3, 3], [1, 1, 2, 3, 3, 4], [], [0, 0, 0], [1, 2, 2, 2]]
+    t = Timer(f"for t in {query}: Solution().deleteDuplicates(t)", "from __main__ import Solution")
+    print(t.timeit(number=1000))
