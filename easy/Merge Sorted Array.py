1e8ce2e2b9c0b2cc4fcc02c13e2991b81a798f13
daewoong lee <dw.lee832@gmail.com>
2021-04-22 22:46:14 +0900

    Merge Sorted Array 문제풀이 01 - 풀이O

---
+++ easy/Merge Sorted Array.py
@@ -0,0 +1,58 @@
+"""
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+The number of elements initialized in nums1 and nums2 are m and n respectively.
+You may assume that nums1 has a size equal to m + n such that it has enough space to hold additional elements from nums2.
+
+Example 1:
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+
+Example 2:
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+
+Constraints:
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-109 <= nums1[i], nums2[i] <= 109
+"""
+
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        i = 0
+        j = 0
+        while j < n and i-j < m:
+            # print(f"nums1[{i}]: {nums1[i]}, nums2[{j}]: {nums2[j]}, nums1: {nums1}")
+            if nums1[i] > nums2[j]:
+                for z in range(m+j, i, -1):
+                    nums1[z] = nums1[z-1]
+                nums1[i] = nums2[j]
+                j += 1
+            i += 1
+        for z in range(n-j):
+            # print(f"z: {z}, nums1[{i+z}]: {nums1[i+z]}, nums2[{j+z}]: {nums2[j+z]}")
+            nums1[i+z] = nums2[j+z]
+        return nums1
+
+
+s = Solution()
+# print(s.merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3))
+# print(s.merge([1, 1, 1, 1, 0], 4, [2], 1))
+# print(s.merge([2, 3, 4, 5, 0], 4, [1], 1))
+# print(s.merge([1], 1, [], 0))
+# print(s.merge([0], 0, [1], 1))
+# print(s.merge([4, 5, 6, 0, 0, 0], 3, [1, 2, 3], 3))
+print(s.merge([-1, 0, 0, 2, 2, 3, 0, 0, 0], 6, [1, 2, 2], 3))
+print(s.merge([1, 2, 3, 0, 0, 0, 0], 3, [2, 5, 6, 7], 4))
+print(s.merge([-1, 0, 1, 1, 0, 0, 0, 0, 0], 4, [-1, 0, 2, 2, 3], 5))
+print(s.merge([-1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0], 5, [-1, -1, 0, 0, 1, 2], 6))
