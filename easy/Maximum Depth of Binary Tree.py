91cf9136397cce22108f6bfb66e0cc1516a24ea8
daewoong lee <dw.lee832@gmail.com>
2021-04-25 14:19:57 +0900

    Maximum Depth of Binary Tree 문제풀이 01 - code refactoring

--- easy/Maximum Depth of Binary Tree.py
+++ easy/Maximum Depth of Binary Tree.py
@@ -28,12 +28,17 @@ class Solution(object):
         print(nums)
         self.print_tree(new_stack)
 
-    def get_depth(self, r1, r2, depth):
-        if r1 is None and r2 is None:
-            return depth
-        l1 = self.get_depth(r1.left, r1.right, depth+1)
-        l2 = self.get_depth(r2.left, r2.right, depth+1)
-        return l1 if l1 > l2 else l2
+    # 0.0340235
+    # def get_depth(self, r1, r2, depth):
+    #     if r1 is None and r2 is None:
+    #         return depth
+    #     l1 = self.get_depth(r1.left, r1.right, depth+1)
+    #     l2 = self.get_depth(r2.left, r2.right, depth+1)
+    #     return l1 if l1 > l2 else l2
+
+    # code refactoring - 0.027760454999999996
+    def get_depth(self, r):
+        return 0 if r is None else 1 + max(self.get_depth(r.left), self.get_depth(r.right))
 
     def maxDepth(self, root):
         """
@@ -42,21 +47,28 @@ class Solution(object):
         """
         head = TreeNode(root[0])
         self.create_tree(root, head, 1)
-        self.print_tree([head])
+        # self.print_tree([head])
 
         if not head:
             return 0
         elif not head.left and not head.right:
             return 1
         else:
-            return self.get_depth(head.left, head.right, 1)
+            # return self.get_depth(head.left, head.right, 1)
+            return self.get_depth(head)
 
 
 s = Solution()
-# print(s.maxDepth([3, 9, 20, None, None, 15, 7]))
-# print(s.maxDepth([3, 9, 20, 10, None, 15, 7, None, None, None, None, None, None, None, 9]))
+print(s.maxDepth([3, 9, 20, None, None, 15, 7]))
+print(s.maxDepth([3, 9, 20, 10, None, 15, 7, None, None, None, None, None, None, None, 9]))
 print(s.maxDepth([1, None, 2]))
 # print(s.maxDepth([]))
 
 
-
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [[[3, 9, 20, None, None, 15, 7]],
+             [[3, 9, 20, 10, None, 15, 7, None, None, None, None, None, None, None, 9]],
+             [[1, None, 2]]]
+    t = Timer(f"for t in {query}: Solution().maxDepth(t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
