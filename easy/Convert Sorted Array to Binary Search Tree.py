0380bfe046cbeb7e61e244b197beb821aafacf7f
daewoong lee <dw.lee832@gmail.com>
2021-04-27 22:51:17 +0900

    Convert Sorted Array to Binary Search Tree 문제풀이 02 - 풀이O (접근방법 참조)

--- easy/Convert Sorted Array to Binary Search Tree.py
+++ easy/Convert Sorted Array to Binary Search Tree.py
@@ -7,62 +7,90 @@ class TreeNode(object):
 
 
 class Solution(object):
-    def get_height(self, node, h=0):
-        if node:
-            l = self.get_height(node.left)
-            r = self.get_height(node.right)
-            h = max(l, r) + 1
-        return h
+    # def get_height(self, node, h=0):
+    #     if node:
+    #         l = self.get_height(node.left)
+    #         r = self.get_height(node.right)
+    #         h = max(l, r) + 1
+    #     return h
+    #
+    # def get_factor(self, root):
+    #     l = self.get_height(root.left)
+    #     r = self.get_height(root.right)
+    #     return l-r
+    #
+    # def rotated_rr(self, root):
+    #     pnode = root
+    #     cnode = root.right
+    #     pnode.right = cnode.left
+    #     cnode.left = pnode
+    #     root = cnode
+    #     return root
+    #
+    # def rotated_ll(self, root):
+    #     pnode = root
+    #     cnode = root.left
+    #     pnode.left = cnode.right
+    #     cnode.right = pnode
+    #     root = cnode
+    #     return root
+    #
+    # def balance(self, root):
+    #     factor = self.get_factor(root)
+    #     if factor > 1:
+    #         root = self.rotated_ll(root)
+    #     elif factor < -1:
+    #         root = self.rotated_rr(root)
+    #     return root
+    #
+    # def insert(self, root, node):
+    #     if root:
+    #         if node.val < root.val:
+    #             if root.left is None:
+    #                 root.left = node
+    #             else:
+    #                 root.left = self.insert(root.left, node)
+    #         else:
+    #             if root.right is None:
+    #                 root.right = node
+    #             else:
+    #                 root.right = self.insert(root.right, node)
+    #         root = self.balance(root)
+    #     else:
+    #         root = node
+    #     return root
+    #
+    # def print_tree(self, root):
+    #     if root:
+    #         print(root.val)
+    #         self.print_tree(root.left)
+    #         self.print_tree(root.right)
+    #
+    # def sortedArrayToBST(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: TreeNode
+    #     """
+    #     root = None
+    #     for i, n in enumerate(nums):
+    #         root = self.insert(root, TreeNode(n))
+    #     self.print_tree(root)
+    #     return root
 
-    def get_factor(self, root):
-        l = self.get_height(root.left)
-        r = self.get_height(root.right)
-        return l-r
-
-    def rotated_rr(self, root):
-        pnode = root
-        cnode = root.right
-        pnode.right = cnode.left
-        cnode.left = pnode
-        root = cnode
-        return root
-
-    def rotated_ll(self, root):
-        pnode = root
-        cnode = root.left
-        pnode.left = cnode.right
-        cnode.right = pnode
-        root = cnode
-        return root
-
-    def balance(self, root):
-        factor = self.get_factor(root)
-        if factor > 1:
-            root = self.rotated_ll(root)
-        elif factor < -1:
-            root = self.rotated_rr(root)
-        return root
-
-    def insert(self, root, node):
-        if root:
-            if node.val < root.val:
-                if root.left is None:
-                    root.left = node
-                else:
-                    root.left = self.insert(root.left, node)
-            else:
-                if root.right is None:
-                    root.right = node
-                else:
-                    root.right = self.insert(root.right, node)
-            root = self.balance(root)
+    def insert(self, nums):
+        if len(nums) == 1:
+            return TreeNode(nums[0])
+        elif len(nums) == 0:
+            return None
         else:
-            root = node
-        return root
+            root = TreeNode(nums[len(nums)//2])
+            root.left = self.insert(nums[:len(nums)//2])
+            root.right = self.insert(nums[len(nums)//2+1:])
+            return root
 
     def print_tree(self, root):
         if root:
-            print(root.val)
+            print(f"val: {root.val}")
             self.print_tree(root.left)
             self.print_tree(root.right)
 
@@ -71,14 +99,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: TreeNode
         """
-        root = None
-        for i, n in enumerate(nums):
-            root = self.insert(root, TreeNode(n))
+        root = TreeNode(nums[len(nums)//2])
+        root.left = self.insert(nums[:len(nums)//2])
+        root.right = self.insert(nums[len(nums)//2+1:])
         self.print_tree(root)
         return root
 
 
 s = Solution()
-# print(s.sortedArrayToBST([-10, -3, 0, 5, 9]))
+print(s.sortedArrayToBST([-10, -3, 0, 5, 9]))
 # print(s.sortedArrayToBST([-10, -3, 0]))
-print(s.sortedArrayToBST([5, 7, 9]))
+# print(s.sortedArrayToBST([5, 7, 9]))
