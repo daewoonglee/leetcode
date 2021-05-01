03e5a4791677b99cf88a80459debeb44dfa7a410
daewoong lee <dw.lee832@gmail.com>
2021-05-01 13:10:07 +0900

    Minimum Depth of Binary Tree 문제풀이 02 - code refactoring

--- easy/Minimum Depth of Binary Tree.py
+++ easy/Minimum Depth of Binary Tree.py
@@ -8,7 +8,7 @@ class TreeNode(object):
 
 class Solution(object):
     def create_node(self, nums, idx=1):
-        if idx > len(nums):
+        if idx > len(nums) or nums[idx-1] is None:
             return None
         root = TreeNode(nums[idx-1])
         root.left = self.create_node(nums, idx*2)
@@ -24,11 +24,35 @@ class Solution(object):
     def get_height(self, root):
         if root is None:
             return 0
-        if root.val is None:
-            return 0
         l = self.get_height(root.left)
         r = self.get_height(root.right)
-        return min(l, r) + 1 if l+r > 2 and l and r else max(l, r) + 1
+
+        # 0.284066799
+        # return min(l, r) + 1 if l+r > 2 and l and r else max(l, r) + 1
+
+        # code refactoring 01 - 0.270606075
+        return max(l, r) + 1 if not l or not r else min(l, r) + 1
+
+    # code refactoring 02 - 0.250741706
+    def get_height_bfs(self, root):
+        def is_leef(root):
+            return True if root.left is None and root.right is None else False
+
+        if root is None:
+            return 0
+        search_stack = [root]
+        depth = 1
+        while 1:
+            new_stack = []
+            for node in search_stack:
+                if is_leef(node):
+                    return depth
+                if node.left:
+                    new_stack.append(node.left)
+                if node.right:
+                    new_stack.append(node.right)
+            search_stack = new_stack
+            depth += 1
 
     def minDepth(self, root):
         """
@@ -37,10 +61,27 @@ class Solution(object):
         """
         root = self.create_node(root)
         # self.print_node(root)
-        return self.get_height(root)
+
+        # return self.get_height(root)
+        return self.get_height_bfs(root)
 
 
 s = Solution()
-# print(s.minDepth([3, 9, 20, None, None, 15, 7]))
-# print(s.minDepth([2, None, 3, None, 4, None, 5, None, 6]))
-print(s.minDepth([2, None, 3, None, None, None, 4, None, None, None, None, None, None, None, 5]))
+# print(s.minDepth([3, 9, 20, None, None, 15, 7]))    # 2
+# print(s.minDepth([2, None, 3, None, 4, None, 5, None, 6]))  # 3
+# print(s.minDepth([1, 2, 2, 3])) # 2
+# print(s.minDepth([2, None, 3, None, None, None, 4, None, None, None, None, None, None, None, 5])) # 4
+print(s.minDepth([]))   # 0
+print(s.minDepth([1]))  # 1
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [[3, 9, 20, None, None, 15, 7],
+             [2, None, 3, None, 4, None, 5, None, 6],
+             [1, 2, 2, 3],
+             [2, None, 3, None, None, None, 4, None, None, None, None, None, None, None, 5],
+             [1],
+             []]
+    t = Timer(f"for t in {query}: Solution().minDepth(t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
