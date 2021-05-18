91e2e23ed8956296016bf64141b54a522555e9e8
daewoong lee <dw.lee832@gmail.com>
2021-05-18 22:10:31 +0900

    Single Number 문제풀이 01 - code refactoring

--- easy/Single Number.py
+++ easy/Single Number.py
@@ -4,14 +4,35 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums.sort()
-        for i in range(0, len(nums)-1, 2):
-            if nums[i] != nums[i+1]:
-                return nums[i]
-        return nums[-1]
+        # 0.04456803299999999
+        # nums.sort()
+        # for i in range(0, len(nums)-1, 2):
+        #     if nums[i] != nums[i+1]:
+        #         return nums[i]
+        # return nums[-1]
+
+        # code refactoring 01 - 0.039722647
+        # return sum(set(nums)) * 2 - sum(nums)
+
+        # code refactoring 02 - 0.026619917999999996
+        ans = 0
+        for n in nums:
+            ans ^= n
+        return ans
 
 
 s = Solution()
 print(s.singleNumber([2, 2, 1]))
 print(s.singleNumber([1]))
 print(s.singleNumber([4, 1, 2, 1, 2]))
+print(s.singleNumber([1, 2, 3, 4, 5, 4, 3, 2, 1]))
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [[2, 2, 1],
+             [1],
+             [4, 1, 2, 1, 2],
+             [1, 2, 3, 4, 5, 4, 3, 2, 1]]
+    t = Timer(f"for t in {query}: Solution().singleNumber(t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
