61776bb97d76ca8d0b1c73159489c1521a9cb47e
daewoong lee <dw.lee832@gmail.com>
2021-05-22 14:41:57 +0900

    Binary Tree Preorder Traversal 문제풀이 01 - code refactoring

--- easy/Binary Tree Preorder Traversal.py
+++ easy/Binary Tree Preorder Traversal.py
@@ -15,12 +15,29 @@ class Solution(object):
         root.right = self.create_tree(nums, idx*2+1)
         return root
 
+    # 0.13815577499999998
+    # def get_pre_order(self, root, ans):
+    #     if root:
+    #         ans.append(root.val)
+    #         self.get_pre_order(root.left, ans)
+    #         self.get_pre_order(root.right, ans)
+    #     return ans
+    #
+    # def preorderTraversal(self, root):
+    #     """
+    #     :type root: TreeNode
+    #     :rtype: List[int]
+    #     """
+    #     head = self.create_tree(root)
+    #     ans = self.get_pre_order(head, [])
+    #     return ans
+
+    # code refactoring - 0.131090362
     def get_pre_order(self, root, ans):
         if root:
             ans.append(root.val)
             self.get_pre_order(root.left, ans)
             self.get_pre_order(root.right, ans)
-        return ans
 
     def preorderTraversal(self, root):
         """
@@ -28,7 +45,8 @@ class Solution(object):
         :rtype: List[int]
         """
         head = self.create_tree(root)
-        ans = self.get_pre_order(head, [])
+        ans = []
+        self.get_pre_order(head, ans)
         return ans
 
 
@@ -38,3 +56,14 @@ s = Solution()
 # print(s.preorderTraversal([1]))
 # print(s.preorderTraversal([1, 2]))
 print(s.preorderTraversal([1, None, 2]))
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [[1, None, 2, None, None, 3],
+             [],
+             [1],
+             [1, 2],
+             [1, None, 2]]
+    t = Timer(f"for t in {query}: Solution().preorderTraversal(t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
