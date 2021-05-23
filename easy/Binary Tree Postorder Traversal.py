77c3a3767acb73a30f9cbabbc047d870bc894e6c
daewoong lee <dw.lee832@gmail.com>
2021-05-23 14:28:37 +0900

    Binary Tree Postorder Traversal 문제풀이 01 - code refactoring

--- easy/Binary Tree Postorder Traversal.py
+++ easy/Binary Tree Postorder Traversal.py
@@ -22,20 +22,47 @@ class Solution(object):
             ans.append(root.val)
         return ans
 
+    def print_post_order_r(self, root, ans):
+        if root:
+            if root.left:
+                self.print_post_order(root.left, ans)
+            if root.right:
+                self.print_post_order(root.right, ans)
+            ans.append(root.val)
+        return ans
+
     def postorderTraversal(self, root):
         """
         :type root: TreeNode
         :rtype: List[int]
         """
         head = self.create_tree(root)
+
         ans = []
-        self.print_post_order(head, ans)
+
+        # 0.132638456
+        # self.print_post_order(head, ans)
+
+        # code refactoring - 0.128562277
+        self.print_post_order_r(head, ans)
+
         return ans
 
 
 s = Solution()
-print(s.postorderTraversal([1, None, 2, None, None, 3]))
-print(s.postorderTraversal([]))
-print(s.postorderTraversal([1]))
-print(s.postorderTraversal([1, 2]))
+# print(s.postorderTraversal([1, None, 2, None, None, 3]))
+# print(s.postorderTraversal([]))
+# print(s.postorderTraversal([1]))
+# print(s.postorderTraversal([1, 2]))
 print(s.postorderTraversal([1, None, 2]))
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [[1, None, 2, None, None, 3],
+             [],
+             [1],
+             [1, 2],
+             [1, None, 2]]
+    t = Timer(f"for t in {query}: Solution().postorderTraversal(t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
