bc84b6eed9a8b23d4183aef6e468612271184145
daewoong lee <dw.lee832@gmail.com>
2021-05-29 16:36:59 +0900

    Min Stack 문제풀이 01 - code refactoring

--- easy/Min Stack.py
+++ easy/Min Stack.py
@@ -1,39 +1,60 @@
 class MinStack(object):
+    # 0.025315284729003906
+    # def __init__(self):
+    #     self.stack = []
+    #
+    # def push(self, val):
+    #     self.stack.append(val)
+    #
+    # def pop(self):
+    #     self.stack.pop()
+    #
+    # def top(self):
+    #     return self.stack[-1]
+    #
+    # def getMin(self):
+    #     return min(self.stack)
 
+    # code refactoring - 0.03282785415649414
+    # def __init__(self):
+    #     self.stack = []
+    #     self.min_nums = []
+    #
+    # def push(self, val):
+    #     self.stack.append(val)
+    #     if not self.min_nums or self.min_nums[-1] > val:
+    #         self.min_nums.append(val)
+    #     else:
+    #         self.min_nums.append(self.min_nums[-1])
+    #
+    # def pop(self):
+    #     self.stack.pop()
+    #     self.min_nums.pop()
+    #
+    # def top(self):
+    #     return self.stack[-1]
+    #
+    # def getMin(self):
+    #     return self.min_nums[-1]
+
+    # code refactoring (R) - 0.02877187728881836
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
         self.stack = []
 
-
     def push(self, val):
-        """
-        :type val: int
-        :rtype: None
-        """
-        self.stack.append(val)
-
+        if self.stack:
+            self.stack.append([val, min(val, self.stack[-1][1])])
+        else:
+            self.stack.append([val, val])
 
     def pop(self):
-        """
-        :rtype: None
-        """
         self.stack.pop()
 
-
     def top(self):
-        """
-        :rtype: int
-        """
-        return self.stack[-1]
-
+        return self.stack[-1][0]
 
     def getMin(self):
-        """
-        :rtype: int
-        """
-        return min(self.stack)
+        return self.stack[-1][1]
 
 
 # ["MinStack","push","push","push","getMin","pop","top","getMin"], [[],[-2],[0],[-3],[],[],[],[]]
@@ -46,3 +67,17 @@ obj.pop()
 print(obj.top())
 print(obj.getMin())
 
+if __name__ == '__main__':
+    import time
+    start = time.time()
+    for _ in range(10000):
+        obj = MinStack()
+        obj.push(-2)
+        obj.push(0)
+        obj.push(-3)
+        obj.getMin()
+        obj.pop()
+        obj.top()
+        obj.getMin()
+        del obj
+    print(f"running time: {time.time() - start}")
