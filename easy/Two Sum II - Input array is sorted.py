304bbc3f333755f3d6a97073c1337870957b4301
daewoong lee <dw.lee832@gmail.com>
2021-06-04 22:09:20 +0900

    Two Sum II - Input array is sorted 문제풀이 02 - code refactoring

--- easy/Two Sum II - Input array is sorted.py
+++ easy/Two Sum II - Input array is sorted.py
@@ -5,29 +5,41 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # Time Limit Exceeded
+        # 0.12534178699999998
         # N = len(numbers)
-        # for i, n in enumerate(numbers[:-1]):
-        #     num = target - n
-        #     for j in range(i+1, N):
-        #         if num == numbers[j]:
-        #             return [i+1, j+1]
+        # for i in range(N-1):
+        #     num = target - numbers[i]
+        #     L = i+1
+        #     R = N-1
+        #     while L <= R:
+        #         pivot = (R+L)//2
+        #         pn = numbers[pivot]
+        #         # print(f"pivot: {pivot}, pn: {pn}")
+        #         if num < pn:
+        #             R = pivot-1
+        #         elif num > pn:
+        #             L = pivot+1
+        #         else:
+        #             return [i+1, pivot+1]
 
-        N = len(numbers)
-        for i in range(N-1):
-            num = target - numbers[i]
-            L = i+1
-            R = N-1
-            while L <= R:
-                pivot = (R+L)//2
-                pn = numbers[pivot]
-                print(f"pivot: {pivot}, pn: {pn}")
-                if num < pn:
-                    R = pivot-1
-                elif num > pn:
-                    L = pivot+1
-                else:
-                    return [i+1, pivot+1]
+        # code refactoring 01 - 0.084737223
+        # L = 0
+        # R = len(numbers)-1
+        # while L <= R:
+        #     n = numbers[L] + numbers[R]
+        #     if n < target:
+        #         L += 1
+        #     elif n > target:
+        #         R -= 1
+        #     else:
+        #         return [L+1, R+1]
+
+        # code refactoring 02 - 0.101679039
+        logs = {}
+        for i, n in enumerate(numbers):
+            if n in logs:
+                return [logs[n]+1, i+1]
+            logs[target - n] = i
 
 
 s = Solution()
@@ -38,6 +50,21 @@ s = Solution()
 # print(s.twoSum([-1, 0], -1))            # [1,2]
 # print(s.twoSum([-5, -5, -1, 0], -10))   # [1,2]
 
-# print(s.twoSum([-3, 3, 4, 90], 0))      # [1,2]
+print(s.twoSum([-3, 3, 4, 90], 0))      # [1,2]
 # print(s.twoSum([5, 25, 75], 100))       # [2,3]
-print(s.twoSum([1, 2, 3, 4, 4, 9, 56, 90], 8))  # [4, 5]
+# print(s.twoSum([1, 2, 3, 4, 4, 9, 56, 90], 8))  # [4, 5]
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [[[2, 7, 11, 15], 9],
+             [[2, 3, 4], 6],
+             [[5, 5], 10],
+             [[-1, 0], -1],
+             [[-5, -5, -1, 0], -10],
+             [[-3, 3, 4, 90], 0],
+             [[5, 25, 75], 100],
+             [[1, 2, 3, 4, 4, 9, 56, 90], 8],
+             [[1, 2, 3, 4, 4, 5, 6, 7], 8]]
+    t = Timer(f"for t in {query}: Solution().twoSum(*t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
