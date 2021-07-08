79f049b0cb8feb1fe02f212f71beff88ca70e8aa
daewoong lee <dw.lee832@gmail.com>
2021-07-08 13:55:57 +0900

    Isomorphic Strings 문제풀이 01 - code refactoring

--- easy/Isomorphic Strings.py
+++ easy/Isomorphic Strings.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        # 0.09776170799999999
         log_s, log_t = {}, {}
         for c1, c2 in zip(s, t):
             if c1 not in log_s:
@@ -15,9 +16,75 @@ class Solution(object):
                 return False
         return True
 
+        # 0.08765810399999999
+        # order = {}
+        # rev = {}
+        # for i in range(len(s)):
+        #     if s[i] in order and order[s[i]] != t[i]:
+        #         return False
+        #     else:
+        #         if t[i] in rev:
+        #             return False
+        #         else:
+        #             order[s[i]] = t[i]
+        #             rev[t[i]] = s[i]
+        # return True
+
+        # 0.080993188
+        # mapping = {}
+        # values = []
+        # for i, c in enumerate(s):
+        #     value = t[i]
+        #     if c in mapping and mapping[c] != value:
+        #         return False
+        #     else:
+        #         mapping[c] = value
+        #
+        # for v in mapping.values():
+        #     if v not in values:
+        #         values.append(v)
+        #     else:
+        #         return False
+        # return True
+
+        # 0.093506493
+        # mapping = {}
+        # for i, c in enumerate(s):
+        #     if c not in mapping:
+        #         if t[i] in mapping.values():
+        #             return False
+        #         mapping[c] = t[i]
+        #     elif mapping[c] != t[i]:
+        #         return False
+        # return True
+
+        # 0.077584771
+        hashmap = {}
+        mapval = {}
+        for i in range(len(s)):
+            if s[i] in hashmap and hashmap[s[i]] != t[i]:
+                return False
+            elif t[i] in mapval:
+                return False
+            else:
+                hashmap[s[i]] = t[i]
+                mapval[t[i]] = True
+        return True
+
 
 s = Solution()
 print(s.isIsomorphic("egg", "add"))
 print(s.isIsomorphic("foo", "bar"))
 print(s.isIsomorphic("fow", "baa"))
 print(s.isIsomorphic("paper", "title"))
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [["egg", "add"],
+             ["foo", "bar"],
+             ["fow", "baa"],
+             ["paper", "title"],
+             ["bbbaaaba", "aaabbbba"]]
+    t = Timer(f"for t in {query}: Solution().isIsomorphic(*t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
