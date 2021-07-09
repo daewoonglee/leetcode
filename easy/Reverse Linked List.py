4d694f171670488127214302802ef98d4e5207f1
daewoong lee <dw.lee832@gmail.com>
2021-07-09 23:23:50 +0900

    Reverse Linked List 문제풀이 01 - code refactoring

--- easy/Reverse Linked List.py
+++ easy/Reverse Linked List.py
@@ -30,35 +30,55 @@ class Solution(object):
         head = create_linked_list(head)
         # print_linked_list(head)
 
-        if not head or not head.next:
-            return head
-        elif not head.next.next:
-            temp = head.next
-            head.next.next = head
-            head.next = None
-            head = temp
-            return head
-        else:
-            pre_pts = head
-            head = head.next
-            next_pts = head.next
-            idx = 0
+        # 0.660604941
+        # if not head or not head.next:
+        #     return head
+        # elif not head.next.next:
+        #     temp = head.next
+        #     head.next.next = head
+        #     head.next = None
+        #     head = temp
+        #     return head
+        # else:
+        #     pre_pts = head
+        #     head = head.next
+        #     next_pts = head.next
+        #     idx = 0
+        #
+        #     while next_pts:
+        #         head.next = pre_pts
+        #         if not idx:
+        #             pre_pts.next = None
+        #             idx = 1
+        #         pre_pts = head
+        #         head = next_pts
+        #         next_pts = next_pts.next
+        #     head.next = pre_pts
+        #     return head
 
-            while next_pts:
-                head.next = pre_pts
-                if not idx:
-                    pre_pts.next = None
-                    idx = 1
-                pre_pts = head
-                head = next_pts
-                next_pts = next_pts.next
+        # code refactoring - 0.557256441
+        pre_pts = None
+        while head:
+            next_pts = head.next
             head.next = pre_pts
-            return head
+            pre_pts = head
+            head = next_pts
+        return pre_pts
 
 
 s = Solution()
 # print_linked_list(s.reverseList([1, 2, 3]))
 # print_linked_list(s.reverseList([1, 2, 3, 4, 5]))
-# print_linked_list(s.reverseList([1, 2]))
+print_linked_list(s.reverseList([1, 2]))
 print_linked_list(s.reverseList([1]))
 # print_linked_list(s.reverseList([]))
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [[1, 2, 3],
+             [1, 2, 3, 4, 5],
+             [1, 2],
+             [1],
+             [i for i in range(100)]]
+    t = Timer(f"for t in {query}: Solution().reverseList(t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
