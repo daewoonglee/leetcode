abe6ffb5800b4fa28d30b6f9f5614da8ecf627fa
daewoong lee <dw.lee832@gmail.com>
2021-07-11 13:19:44 +0900

    Concatenation of Array 문제풀이 01 - 풀이O

---
+++ easy/Concatenation of Array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return nums * 2
+
+
+s = Solution()
+print(s.getConcatenation([1, 2, 1]))
