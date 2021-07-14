59e836040f6ac271fa1de3d2485cd6c82dd7b12b
daewoong lee <dw.lee832@gmail.com>
2021-07-14 23:09:13 +0900

    Contains Duplicate II 문제풀이 01 - check process running time

--- easy/Contains Duplicate II.py
+++ easy/Contains Duplicate II.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
+        # 1.9851560299999997
         lookup = dict()
         for i, num in enumerate(nums):
             if num in lookup and i - lookup[num] <= k:
@@ -17,3 +18,16 @@ s = Solution()
 print(s.containsNearbyDuplicate([1, 2, 3, 1], k=3))
 print(s.containsNearbyDuplicate([1, 0, 1, 1], k=1))
 print(s.containsNearbyDuplicate([1, 2, 3, 1, 2, 3], k=2))
+print(s.containsNearbyDuplicate(list(range(1000)) + [0], k=1000))
+print(s.containsNearbyDuplicate(list(range(1000)) + [0], k=999))
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [[[1, 2, 3, 1], 3],
+             [[1, 0, 1, 1], 1],
+             [[1, 2, 3, 1, 2, 3], 2],
+             [list(range(1000)) + [0], 1000],
+             [list(range(1000)) + [0], 999]]
+    t = Timer(f"for t in {query}: Solution().containsNearbyDuplicate(*t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
