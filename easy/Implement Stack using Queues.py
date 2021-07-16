e9a5ea60bff2cbaecc6824cdfe336ee69f0efa5b
daewoong lee <dw.lee832@gmail.com>
2021-07-16 23:48:56 +0900

    Implement Stack using Queues 문제풀이 01 - code refactoring using queue

--- easy/Implement Stack using Queues.py
+++ easy/Implement Stack using Queues.py
@@ -1,10 +1,12 @@
+from collections import deque
 class MyStack(object):
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.nums = list()
+        # self.nums = list()
+        self.nums = deque()
 
     def push(self, x):
         """
@@ -12,7 +14,14 @@ class MyStack(object):
         :type x: int
         :rtype: None
         """
+        # self.nums.append(x)
+        # return None
+
         self.nums.append(x)
+        N = len(self.nums)
+        for _ in range(N-1):
+            n = self.nums.popleft()
+            self.nums.append(n)
         return None
 
     def pop(self):
@@ -20,14 +29,16 @@ class MyStack(object):
         Removes the element on top of the stack and returns that element.
         :rtype: int
         """
-        return self.nums.pop() if self.nums else None
+        # return self.nums.pop() if self.nums else None
+        return self.nums.popleft() if self.nums else None
 
     def top(self):
         """
         Get the top element.
         :rtype: int
         """
-        return self.nums[-1]
+        # return self.nums[-1]
+        return self.nums[0]
 
     def empty(self):
         """
@@ -40,12 +51,12 @@ class MyStack(object):
 # Your MyStack object will be instantiated and called as such:
 obj = MyStack()
 
-# obj.push(1)
-# obj.push(2)
-# param_2 = obj.top()
-# param_3 = obj.pop()
-# param_4 = obj.empty()
-# print(param_2, param_3, param_4)
+obj.push(1)
+obj.push(2)
+param_2 = obj.top()
+param_3 = obj.pop()
+param_4 = obj.empty()
+print(param_2, param_3, param_4)
 
 # ["MyStack", "push", "push", "top", "pop", "empty"]
 # [[], [1], [2], [], [], []]
