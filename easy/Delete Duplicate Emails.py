a83c782cacd6265d37a8bdc87ec9ae75830f97e0
daewoong lee <dw.lee832@gmail.com>
2021-07-17 00:14:36 +0900

    Delete Duplicate Emails 문제풀이 01 - code refactoring

--- easy/Delete Duplicate Emails.py
+++ easy/Delete Duplicate Emails.py
@@ -1,4 +1,17 @@
 """
 delete p1 from person p1, person p2
 where p1.id > p2.id and p1.email = p2.email
+
+
+delete from person p1
+where p1.id not in (select * from (select min(id) from person
+                    group by email) as p2)
+
+
+with cte as
+(
+    select id,email, row_number() over(
+    partition by email order by id) as dups from Person
+)
+delete from person where id in (select id from cte where dups > 1);
 """
