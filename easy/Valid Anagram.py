6833ae86b7a483e9a1977a63d76b1b0ea6797459
daewoong lee <dw.lee832@gmail.com>
2021-08-11 22:24:38 +0900

    Valid Anagram 문제풀이 01 - code refactoring

--- easy/Valid Anagram.py
+++ easy/Valid Anagram.py
@@ -5,7 +5,23 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return sorted(s) == sorted(t)
+        # 0.06722059999999999
+        # return sorted(s) == sorted(t)
+
+        # code refactoring - 0.128549661
+        cnt = dict()
+        for c in s:
+            if c not in cnt:
+                cnt[c] = 0
+            cnt[c] += 1
+        for c in t:
+            if c not in cnt:
+                return False
+            cnt[c] -= 1
+        for v in cnt.values():
+            if v:
+                return False
+        return True
 
 
 s = Solution()
@@ -14,3 +30,14 @@ print(s.isAnagram("stressed", "desserts"))
 print(s.isAnagram("elvis", "lives"))
 print(s.isAnagram("elvis", "livee"))
 print(s.isAnagram("abcdefg", "abcdefgh"))
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [["anagram", "nagaram"],
+             ["stressed", "desserts"],
+             ["elvis", "lives"],
+             ["elvis", "livee"],
+             ["abcdefg", "abcdefgh"]]
+    t = Timer(f"for t in {query}: Solution().isAnagram(*t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
