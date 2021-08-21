520bbbd5b17db05d66aa44211ec2435a0ed75ee9
daewoong lee <dw.lee832@gmail.com>
2021-08-21 17:17:01 +0900

    Add Digits 문제풀이 01 - code refactoring

--- easy/Add Digits.py
+++ easy/Add Digits.py
@@ -4,15 +4,27 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        while num > 9:
-            q, d = -1, -1
-            total = 0
-            while q:
-                q, d = divmod(num, 10)
-                total += d
-                num = q
-            num = total
-        return num
+        # 0.051096737
+        # while num > 9:
+        #     total = 0
+        #     while num:
+        #         num, d = divmod(num, 10)
+        #         total += d
+        #     num = total
+        # return num
+
+        # code refactoring 01 - 0.051261155
+        # ans = 0
+        # while num:
+        #     num, d = divmod(num, 10)
+        #     ans += d
+        #     if num == 0 and ans > 9:
+        #         num = ans
+        #         ans = 0
+        # return ans
+
+        # code refactoring 02 - 0.013917721
+        return 1 + (num - 1) % 9 if num else 0
 
 
 s = Solution()
@@ -20,3 +32,11 @@ print(s.addDigits(11))
 print(s.addDigits(12))
 print(s.addDigits(121))
 print(s.addDigits(49))
+print(s.addDigits(9999))
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [11, 12, 121, 49, 9999]
+    t = Timer(f"for t in {query}: Solution().addDigits(t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
