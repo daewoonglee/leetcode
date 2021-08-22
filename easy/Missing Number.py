544fc52882a0d114d53ad1e68f7558b0ac35e45a
idaeung <cldw1101@onycom.com>
2021-08-22 10:27:13 +0900

    Missing Number 문제풀이 01 - 풀이O

---
+++ easy/Missing Number.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        N = len(nums)
+        nums.sort()
+        for i in range(N):
+            if i != nums[i]:
+                return i
+        return N
+
+s = Solution()
+print(s.missingNumber([3, 0, 1]))
+print(s.missingNumber([9,6,4,2,3,5,7,0,1]))
+print(s.missingNumber([0, 1]))
+
