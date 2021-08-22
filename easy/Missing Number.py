2cd6d9dc00465b02c1a0e5b396e0f25013dfa944
daewoong lee <dw.lee832@gmail.com>
2021-08-22 10:44:46 +0900

    Missing Number 문제풀이 01 - 풀이O

--- easy/Missing Number.py
+++ easy/Missing Number.py
@@ -11,8 +11,8 @@ class Solution(object):
                 return i
         return N
 
+
 s = Solution()
 print(s.missingNumber([3, 0, 1]))
-print(s.missingNumber([9,6,4,2,3,5,7,0,1]))
+print(s.missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
 print(s.missingNumber([0, 1]))
-
