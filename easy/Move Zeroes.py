93d2e2bbe9ba11956396f8a636f6117a0baf18a8
daewoonglee <dw.lee832@gmail.com>
2021-08-24 22:25:02 +0900

    Move Zeroes 문제풀이 01 - 풀이O

---
+++ easy/Move Zeroes.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        N = len(nums)
+        zero = 0
+        while 1:
+            while zero < N and nums[zero]:
+                zero += 1
+            non_zero = zero + 1
+            while non_zero < N and not nums[non_zero]:
+                non_zero += 1
+            if N <= non_zero:
+                break
+            else:
+                temp = nums[non_zero]
+                nums[non_zero] = nums[zero]
+                nums[zero] = temp
+        return nums
+
+
+s = Solution()
+print(s.moveZeroes([0, 1, 0, 3, 12]))
+print(s.moveZeroes([0, 0, 1, 3, 12]))
+print(s.moveZeroes([1, 3, 12, 0, 0]))
+print(s.moveZeroes([1, 0, 0, 2, 0]))
+print(s.moveZeroes([1, 2, 0, 3, 0]))
+print(s.moveZeroes([1]))
