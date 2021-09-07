afc3f10ba5cb985d1777c1d6658a2ad48d5c80c0
daewoonglee <dw.lee832@gmail.com>
2021-09-07 21:58:47 +0900

    Range Sum Query 문제풀이 01 - code refactoring

--- easy/Range Sum Query - Immutable.py
+++ easy/Range Sum Query - Immutable.py
@@ -4,8 +4,13 @@ class NumArray(object):
         """
         :type nums: List[int]
         """
-        self.nums = nums        
-        self.log = dict()
+        # self.nums = nums        
+        # self.log = dict()
+        
+        # code refactoring (R)
+        self.sum_list = [0]
+        for i, n in enumerate(nums):
+            self.sum_list.append(self.sum_list[-1]+n)        
 
     def sumRange(self, left, right):
         """
@@ -13,14 +18,16 @@ class NumArray(object):
         :type right: int
         :rtype: int
         """
-        if left in self.log:
-            if right in self.log[left]:
-                return self.log[left][right]
-        else:
-            self.log[left] = dict()
-        self.log[left][right] = sum(self.nums[left: right+1])
-        return self.log[left][right]
+        # if left in self.log:
+        #     if right in self.log[left]:
+        #         return self.log[left][right]
+        # else:
+        #     self.log[left] = dict()
+        # self.log[left][right] = sum(self.nums[left: right+1])
+        # return self.log[left][right]
 
+        # code refactoring (R)
+        return self.sum_list[right+1] - self.sum_list[left]
 
 obj = NumArray([-2, 0, 3, -5, 2, -1])
 param_1 = obj.sumRange(0, 2)
