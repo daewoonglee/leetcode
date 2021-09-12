94e6c5ab7e1d2aa40bf64ae6741bf42db0140d4c
daewoonglee <dw.lee832@gmail.com>
2021-09-12 11:22:24 +0900

    Counting Bits 문제풀이 01 - code refactoring 02

--- easy/Counting Bits.py
+++ easy/Counting Bits.py
@@ -8,13 +8,25 @@ class Solution(object):
         # return [bin(i)[2:].count("1") for i in range(n+1)]
 
         # code refactoring - 1.5244826506823301        
-        N = 0
-        while 2**N < n:
-            N += 1
-        li = [0, 1]
-        for _ in range(N):
-            li += [i+1 for i in li]
-        return li[:n+1]
+        # N = 0
+        # while 2**N < n:
+        #     N += 1
+        # li = [0, 1]
+        # for _ in range(N):
+        #     li += [i+1 for i in li]
+        # return li[:n+1]
+
+        # code refactoring (R) - 0.8218544516712427
+        # li = [0]
+        # while len(li) < n+1:
+        #     li += [i+1 for i in li]
+        # return li[:n+1]
+
+        # code refactoring (R) - 2.0805573109537363
+        dp = [0] * (n+1)
+        for i in range(1, n+1):
+            dp[i] = (i & 1) + dp[i >> 1]
+        return dp
 
 
 s = Solution()
