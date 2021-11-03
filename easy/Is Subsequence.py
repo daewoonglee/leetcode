6bdee748e714fb25e8005639d2485b47f613be70
daewoonglee <dw.lee832@gmail.com>
2021-11-03 23:33:36 +0900

    Is Subsequence 문제풀이 01 - code refactoring

--- easy/Is Subsequence.py
+++ easy/Is Subsequence.py
@@ -5,16 +5,39 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        i = 0
-        for ch in t:
-            if i == len(s):
-                break
-            if s[i] == ch:
-                i += 1
-        return i == len(s)
-        
+        # 0.06754814414307475
+        #i = 0
+        #N = len(s)
+        #for ch in t:
+        #    if i == N:
+        #        break
+        #    if s[i] == ch:
+        #        i += 1
+        #return i == len(s)
+
+        # code refactoring (R) - 0.09510712698101997
+        for i in s:
+            j = t.find(i)
+            if j == -1:
+                return False
+            else:
+                # 메모리할당이 다시 일어나서 느림 
+                # 접근방법 참고 겸 기재 
+                t = t[j+1:]
+        return True
+
 
 s = Solution()
 print(s.isSubsequence("abc", "ahbgdc"))
 print(s.isSubsequence("abc", "ahchb"))
+print(s.isSubsequence("aa", "a"))
+print(s.isSubsequence("aa", "aaa"))
+print(s.isSubsequence("", "a"))
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [["abc", "ahbgdc"], ["abc", "ahchb"], ["aa", "a"], ["aa", "aaa"], ["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "abbbbbbbbbbbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"]]
+    t = Timer(f"for t in {query}: Solution().isSubsequence(*t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
 
