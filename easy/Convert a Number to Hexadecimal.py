87be4ff556f7cacf27752004412db131b55d833e
daewoonglee <dw.lee832@gmail.com>
2021-11-10 23:51:14 +0900

    Convert a Number to Hexadecimal 문제풀이 02 - code refactoring

--- easy/Convert a Number to Hexadecimal.py
+++ easy/Convert a Number to Hexadecimal.py
@@ -4,16 +4,35 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
+        # 0.24172726273536682
+        #hex_li = "0123456789abcdef"
+        #hex_num=[]
+        #if num < 0:
+        #    #num = (2**32-1)+num+1
+
+        #    # code refactoring (R) 01 - 0.24057982303202152
+        #    num &= (2**32-1)
+        #elif num == 0:
+        #    return "0"
+        #while num > 0:
+        #    #num, d = divmod(num, 16)
+        #    #hex_num.append(hex_li[d])
+
+        #    # code refactoring (R) 02 - 0.19624112080782652 (call function이 없어서)
+        #    hex_num.append(hex_li[num % 16])
+        #    num //= 16
+        #return ''.join(hex_num[::-1])
+
+        # code refactoring (R) 03 - 0.21813468914479017
         hex_li = "0123456789abcdef"
-        # print(f"num: {num}, d: {num//16}, mul: {16 * (num//16)}, hex: {hex(num)}")
-        hex_num=[]
+        hex_num = []
         if num < 0:
-            num = (2**32-1)+num+1
+            num &= (2**32-1)
         elif num == 0:
             return "0"
         while num > 0:
-            num, d = divmod(num, 16)
-            hex_num.append(hex_li[d])
+            hex_num.append(hex_li[num & 15])
+            num >>= 4
         return ''.join(hex_num[::-1])
 
 
@@ -28,7 +47,7 @@ s = Solution()
 #print(s.toHex(255))
 #print(s.toHex(256))
 #print(s.toHex(2**12-1))
-#print(s.toHex(2**31-1))
+print(s.toHex(2**31-1))
 print(s.toHex(0))
 print(s.toHex(-1))
 print(s.toHex(-2))
@@ -36,4 +55,11 @@ print(s.toHex(-3))
 print(s.toHex(-15))
 print(s.toHex(-16))
 print(s.toHex(-17))
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [26, 15, 16, 17, 32, 33, 160, 255, 256, 2**12-1, 2**31-1, 0, -1, -2, -3, -15, -16, -17, -2**30]
+    t = Timer(f"for t in {query}: Solution().toHex(t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
 
