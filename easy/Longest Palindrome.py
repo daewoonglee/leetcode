e517a3880e3a72f9428e6c76e79ee9fae034a7fb
daewoonglee <dw.lee832@gmail.com>
2021-11-14 23:02:29 +0900

    Longest Palindrome 문제풀이 01 - 풀이O

--- easy/Longest Palindrome.py
+++ easy/Longest Palindrome.py
@@ -12,9 +12,16 @@ class Solution(object):
         ans = 0
         is_one = False
         for v in data.values():
-            if v%2==0: ans += v
-            else: ans += v-1; is_one = True
+            # 0.06960721500217915
+            #if v%2==0: ans += v
+            #else: ans += v-1; is_one = True
+
+            # code refactoring (R) - 0.06984644429758191
+            ans += (v // 2 * 2)
+            if not is_one and v % 2 == 1: is_one = True
         return ans+1 if is_one else ans
+
+
 
 
 s = Solution()
@@ -23,4 +30,11 @@ print(s.longestPalindrome("a"))
 print(s.longestPalindrome("abcde"))
 print(s.longestPalindrome("aaabbbccccdd"))
 print(s.longestPalindrome("aaabccccdd"))
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = ["abccccdd", "a", "abcde", "aaabbbccccdd", "aaabccccdd"]
+    t = Timer(f"for t in {query}: Solution().longestPalindrome(t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
 
