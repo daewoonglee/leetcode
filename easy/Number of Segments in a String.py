77e37ddd850475a9eefdb084965d263b8509b844
daewoonglee <dw.lee832@gmail.com>
2021-11-21 11:10:16 +0900

    Number of Segments in a String 문제풀이 01 - code refactoring

--- easy/Number of Segments in a String.py
+++ easy/Number of Segments in a String.py
@@ -4,7 +4,30 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        return len(s.split())
+        # 0.017453459091484547
+        #return len(s.split())
+
+        # without built-in function - 0.05456833727657795
+        #if s == "":
+        #    return 0
+        #cnt = 0
+        #for i, ch in enumerate(s[:-1]):
+        #    if ch == " " and s[i+1] != " ":
+        #        cnt += 1
+        #return cnt if s[0] == " " else cnt + 1
+
+        # without built-in function code refactoring (R) - 0.04684274364262819
+        if not len(s):
+            return 0
+        count = 0
+        is_blank = True
+        for ch in s:
+            if not ch == " " and is_blank:
+                count += 1
+                is_blank = False
+            elif ch == " ":
+                is_blank = True
+        return count
 
 
 s = Solution()
@@ -12,4 +35,13 @@ print(s.countSegments("Hello, my name is John"))
 print(s.countSegments("Hello"))
 print(s.countSegments("love live! mu'sic forever"))
 print(s.countSegments(""))
+print(s.countSegments("   G iii"))
+print(s.countSegments("   G      "))
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = ["Hello, my name is John", "Hello", "love live! mu'sic forever", "", "    for iiii"]
+    t = Timer(f"for t in {query}: Solution().countSegments(t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
         
