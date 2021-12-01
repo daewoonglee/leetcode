578ff5bf3ff4edb8cd180251e0a7d0b3672dbe03
daewoonglee <dw.lee832@gmail.com>
2021-12-01 10:12:57 +0900

    Assign Cookies 문제풀이 01 - 풀이 O

---
+++ easy/Assign Cookies.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        g.sort()
+        s.sort()
+        ans = i = j = 0
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                ans += 1
+                i += 1
+            j += 1
+        return ans
+
+
+s = Solution()
+print(s.findContentChildren([1,2,3], [1,1]))
+print(s.findContentChildren([1,2,3], [3]))
+print(s.findContentChildren([1,2,3], [2,3]))
+print(s.findContentChildren([1,1,1], [2,3,4]))
+
