c3272e2602bf9693780c1aaef7ea724d03b6d497
daewoonglee <dw.lee832@gmail.com>
2021-12-04 16:22:04 +0900

    Repeated Substring Pattern 문제풀이 02 - code refactoring 02

--- easy/Repeated Substring Pattern.py
+++ easy/Repeated Substring Pattern.py
@@ -11,11 +11,39 @@ class Solution(object):
         #return False
         
         # code refactoring - 0.21546904556453228 
-        for i in range(1, len(s)//2+1):
-            if len(s) % i == 0:
-                split_s = [s[j: j+i] for j in range(0, len(s), i)]
-                if len(set(split_s)) == 1: return True
-        return False
+        #for i in range(1, len(s)//2+1):
+        #    if len(s) % i == 0:
+        #        split_s = [s[j: j+i] for j in range(0, len(s), i)]
+        #        if len(set(split_s)) == 1: return True
+        #return False
+
+        # code refactoring 02 - 0.154229449108243
+        #N = len(s)
+        #li = []
+        #for i in range(1, N//2+1):
+        #    if N % i == 0:
+        #        li.append(s[:i])
+        #        for j in range(i, N, i):
+        #            li.append(s[j:j+i])
+        #            if li[-1] != li[-2]: break
+        #        if li[-1] == li[-2]: return True
+        #        # clear time complexity = O(N)
+        #        li.clear()
+        #return False
+
+        # code refactoring 02-1 - 0.14705622009932995
+        #N = len(s)
+        #for i in range(1, N//2+1):
+        #    if N % i == 0:
+        #        li = [s[:i]]
+        #        for j in range(i, N, i):
+        #            li.append(s[j:j+i])
+        #            if li[-1] != li[-2]: break
+        #        if li[-1] == li[-2]: return True
+        #return False
+
+        # code refactoring (R) - 0.030461512506008148        
+        return s in (s+s)[1:-1]
 
 
 s = Solution()
@@ -23,12 +51,14 @@ s = Solution()
 #print(s.repeatedSubstringPattern("aa"))
 #print(s.repeatedSubstringPattern("ab"))
 #print(s.repeatedSubstringPattern("abab"))
-print(s.repeatedSubstringPattern("abac"))
-print(s.repeatedSubstringPattern("abababac"))
+#print(s.repeatedSubstringPattern("abac"))
+#print(s.repeatedSubstringPattern("abababac"))
+#print(s.repeatedSubstringPattern("ababab"))
 #print(s.repeatedSubstringPattern("abcab"))
 #print(s.repeatedSubstringPattern("aabaaba"))
-#print(s.repeatedSubstringPattern("aaaaa"))
-#print(s.repeatedSubstringPattern("abcabcabc"))
+print(s.repeatedSubstringPattern("aaaaa"))
+print(s.repeatedSubstringPattern("abcabcabc"))
+print(s.repeatedSubstringPattern("abacabac"))
 
 
 if __name__ == '__main__':
