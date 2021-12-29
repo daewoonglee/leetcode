c7cd4cb25a71653a0708532645647e68ffbe30c0
daewoonglee <dw.lee832@gmail.com>
2021-12-29 22:59:42 +0900

    Longest Uncommon Subsequence I - code refactoring

--- easy/Longest Uncommon Subsequence I.py
+++ easy/Longest Uncommon Subsequence I.py
@@ -5,9 +5,22 @@ class Solution(object):
         :type b: str
         :rtype: int
         """
-        if a == b:
+        #if a == b:
+        #    return -1
+        # 0.036305010318756104
+        #return len(a) if len(a) > len(b) else len(b)
+        #
+        # 0.04171966575086117 - function callback
+        #return max(len(a), len(b))
+
+        # code refactoring - 0.8633379116654396 (위 a==b 구현)
+        N = len(a) if len(a) > len(b) else len(b)
+        if len(a) == len(b):
+            for ch_a, ch_b in zip(a, b):
+                if ch_a != ch_b:
+                    return N
             return -1
-        return len(a) if len(a) > len(b) else len(b)
+        return N
 
 
 s = Solution()
@@ -19,4 +32,10 @@ print(s.findLUSlength("aabbb", "ccc"))
 print(s.findLUSlength("aabbb", "aa"))
 print(s.findLUSlength("aabbb", "aab"))
 
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [["aaa", "bbb"], ["aaa", "ababa"], ["aaab", "ababab"], ["aaac", "ababa"], ["aabbb", "ccc"], ["aabbb", "aa"], ["aabbb", "aab"], ["a"*1000, "a"*1000], ["a"*1000, "a"*999 + "b"]]
+    t = Timer(f"for t in {query}: Solution().findLUSlength(*t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
 
