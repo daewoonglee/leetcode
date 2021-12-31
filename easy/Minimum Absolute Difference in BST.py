062250ce1748ca7163d15f27ff3ee1c28e218634
daewoonglee <dw.lee832@gmail.com>
2021-12-31 17:50:32 +0900

    Minimum Absolute Difference in BST 문제풀이 01 - code refactoring

--- easy/Minimum Absolute Difference in BST.py
+++ easy/Minimum Absolute Difference in BST.py
@@ -15,15 +15,37 @@ class Solution(object):
         node.right = self.create_bst(nums, idx=idx*2+1)
         return node
 
+    def helper(self, root,minn,maxx,res):
+        if root is None:
+            return
+
+        self.helper(root.left,minn,root.val,res)
+        self.helper(root.right,root.val,maxx,res)
+
+        res[0] = min(res[0],min(root.val-minn,maxx-root.val))
+
+
+    def getMinimumDifference(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """          
+        res = [float('inf')]
+        self.helper(root,-1000000,1000000,res)
+        return res[0]
+
     def getMinimumDifference(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        root_pts = self.create_bst(root)
-        q = queue.Queue()
-        q.put(root_pts)
-        min_ans = nl = nr = 10 ** 5
+        #root_pts = self.create_bst(root)
+        #q = queue.Queue()
+        #q.put(root_pts)
+        #min_ans = N = 10 ** 5
+
+        """
+        # 1.402242835611105
         while q.qsize():
             node = q.get()
             #print(f"val: {node.val}")
@@ -65,12 +87,57 @@ class Solution(object):
 
             if min_ans > n: min_ans = n
         return min_ans
+        """
+
+        """
+        # code refactoring - 1.3477903213351965
+        while q.qsize():
+            nl = nr = N
+            node = q.get()
+            if node.left:
+                q.put(node.left)
+                if node.left.right:
+                    lnode = node.left
+                    while lnode.right.right:
+                        lnode = lnode.right
+                    nl = node.val - lnode.right.val
+                else:
+                    nl = node.val - node.left.val
+
+            if node.right:
+                q.put(node.right)
+                if node.right.left:
+                    rnode = node.right
+                    while rnode.left.left:
+                        rnode = rnode.left
+                    nr = rnode.left.val - node.val
+                else:
+                    nr = node.right.val - node.val
+
+            diff = nl if nl < nr else nr
+            if min_ans > diff: min_ans = diff
+        return min_ans
+        """
+
+        # code refactoring (R) - 0.358064747415483
+        root_pts = self.create_bst(root)
+        res = [float('inf')]
+        self.helper(root_pts, -1000000, 1000000,res)
+        return res[0]
 
 
 s = Solution()
 #print(s.getMinimumDifference([4, 2, 6, 1, 3, 5]))
 #print(s.getMinimumDifference([1, 0, 48, None, None, 12, 49]))
 #print(s.getMinimumDifference([1, None, 5, None, None, 3]))
-print(s.getMinimumDifference([1, None, 50, None, None, 10]))
+#print(s.getMinimumDifference([1, None, 50, None, None, 10]))
 print(s.getMinimumDifference([10, 1, None, None, 5, None, None, None, None, None, 8]))
+#print(s.getMinimumDifference([4, 2, 6, 1, 3]))
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [[4, 2, 6, 1, 3, 5], [1, 0, 48, None, None, 12, 49], [1, None, 5, None, None, 3], [1, None, 50, None, None, 10], [10, 1, None, None, 5, None, None, None, None, None, 8]]
+    t = Timer(f"for t in {query}: Solution().getMinimumDifference(t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
 
