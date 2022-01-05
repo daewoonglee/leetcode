02ec2cea7ee40afa0745de1e5406906a3d3bb7c3
daewoonglee <dw.lee832@gmail.com>
2022-01-05 21:35:16 +0900

    Diameter of Binary Tree 문제풀이 02 - code refactoring

--- easy/Diameter of Binary Tree.py
+++ easy/Diameter of Binary Tree.py
@@ -21,6 +21,8 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        # 0.21875363029539585
+        """
         def get_edges(pts):
             edges = 0
             stack_edges = [[pts, 1]]
@@ -42,6 +44,20 @@ class Solution(object):
             if node.left: stack_node.append(node.left)
             if node.right: stack_node.append(node.right)
         return max_edges
+        """
+
+        # code refactoring - 0.1862743031233549
+        def get_edges(node):
+            if not node: return [0,0]
+            L, ledges = get_edges(node.left)
+            R, redges = get_edges(node.right)
+            if not L and not R: return [1,0]
+            elif not L: return [R+1,redges+1] if R != redges else [R+1, redges]
+            elif not R: return [L+1,ledges+1] if L != ledges else [L+1, ledges]
+            else: return [max(L,R)+1,max(ledges,redges,L+R)]
+
+        root = self.create_binary_tree(root)
+        return get_edges(root)[1]
 
 
 s = Solution()
@@ -50,4 +66,12 @@ print(s.diameterOfBinaryTree([1, None, 3, None, None, None, 4, None, None, None,
 print(s.diameterOfBinaryTree([1])) # 0
 print(s.diameterOfBinaryTree([1,2])) # 1
 #print(s.diameterOfBinaryTree([4,-7,-3,null,null,-9,-3,9,-7,-4,null,6,null,-6,-6,null,null,0,6,5,null,9,null,null,-1,-4,null,null,null,-2])) # expected 8, output 7
- 
+
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [[1, 2, 3, 4, 5], [1, None, 3, None, None, None, 4, None, None, None, None, None, None, 5, 6], [1], [1, 2]]
+    t = Timer(f"for t in {query}: Solution().diameterOfBinaryTree(t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
+
