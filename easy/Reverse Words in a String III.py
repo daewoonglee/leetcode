c2149352a1867dc2b23f1fc59c0eb8e80d751ccb
daewoonglee <dw.lee832@gmail.com>
2022-01-09 21:44:30 +0900

    Reverse Words in a String III 문제풀이 01 - code refactoring

--- easy/Reverse Words in a String III.py
+++ easy/Reverse Words in a String III.py
@@ -4,12 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        #version1
+        #version1 - 0.14672745391726494
         #return " ".join([line[::-1] for line in s.split()])
 
-        #version2 - 직접구현
-        white_space = ord(" ")
-        space_idx = [i for i, ch in enumerate(s) if ord(ch)==white_space] + [len(s)]
+        #version2 - 직접구현 0.7739312127232552
+        space_idx = [i for i, ch in enumerate(s) if ch==" "] + [len(s)]
 
         ans = ""
         pre_idx = -1
@@ -26,4 +25,11 @@ s = Solution()
 print(s.reverseWords("Let's take LeetCode contest"))
 print(s.reverseWords("God Ding"))       
 print(s.reverseWords("g"))
- 
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = ["Let's take LeetCode contest", "God Ding", "g", "a"*100, "a " * 100]
+    t = Timer(f"for t in {query}: Solution().reverseWords(t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
+
