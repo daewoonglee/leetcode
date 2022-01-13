22e32f44b2da5bb30e23002e6226d72abd356521
daewoonglee <dw.lee832@gmail.com>
2022-01-13 12:05:37 +0900

    Array Partition I 문제풀이 02 - 풀이O

--- easy/Array Partition I.py
+++ easy/Array Partition I.py
@@ -6,16 +6,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # 순서 인지 필요
-        comb = list(combinations(nums, 2))
-        print(f"comb: {comb}")
-        i = 0
-        ans = 0
-        while i < len(comb)//2:
-            s = min(comb[i]) + min(comb[-1-i])
-            if ans < s: ans = s
-            i += 1
-        return ans
+        return sum(sorted(nums)[::2])
 
 
 s = Solution()
