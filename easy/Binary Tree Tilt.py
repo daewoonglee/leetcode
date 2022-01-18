60803ce20e8c1aa655bc655f4c128443a26dc46f
daewoonglee <dw.lee832@gmail.com>
2022-01-18 23:10:59 +0900

    Binary Tree Tilt 문제풀이 01 - code refactoring

--- easy/Binary Tree Tilt.py
+++ easy/Binary Tree Tilt.py
@@ -17,9 +17,14 @@ class Solution(object):
         return node
 
     def search_node(self, root):
-        if not root: return 0
-        L = self.search_node(root.left)
-        R = self.search_node(root.right)
+        # 0.18582716025412083        
+        #if not root: return 0
+        #L = self.search_node(root.left)
+        #R = self.search_node(root.right)
+
+        # code refactoring (callback function) - 0.17429040372371674
+        L = 0 if not root.left else self.search_node(root.left)
+        R = 0 if not root.right else self.search_node(root.right)
         self.ans += abs(L-R)
         return root.val + L + R
 
@@ -28,12 +33,20 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        if not root: return 0
         root = self.create_binary_tree(root)
         self.search_node(root)
         return self.ans
-       
+
 
 s = Solution()
 #print(s.findTilt([1,2,3]))
 print(s.findTilt([4,2,9,3,5,None,7]))
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [[1,2,3], [4,2,9,3,5,None,7], [1,2,None,3,None,None,None,4]]
+    t = Timer(f"for t in {query}: Solution().findTilt(t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
  
