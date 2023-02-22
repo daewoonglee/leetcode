dfd58b0d6f25a3acbdf27a1bdecd962b5a26ee95
daewoong lee <dw.lee832@gmail.com>
2023-02-23 06:48:42 +0900

    Maximum Depth of N-ary Tree 문제풀이 01 - code refactoring

--- easy/Maximum Depth of N-ary Tree.py
+++ easy/Maximum Depth of N-ary Tree.py
@@ -9,79 +9,84 @@ class Node(object):
 
 
 class Solution(object):
-    """
-    create_tree 미완, 우선 get_depth에 대한 문제풀이만 진행
+    def __init__(self):
+        self.idx = 2
 
-    def create_tree(self, r, n, idx):
-        if len(r) > idx:
-            N = len(n) if isinstance(n, list) else 1
+    def create_tree(self, r, n):
+        if len(r) > self.idx:
+            N = len(n)
             for i in range(N):
                 childrens = []
-                for v in r[idx:]:
-                    if not v: break
+                for v in r[self.idx:]:
+                    self.idx += 1
+                    if not v:
+                        break
                     childrens.append(Node(v))
-                    idx += 1
-                if isinstance(n, list):
-                    n[i].children = childrens
-                else:
-                    n.children = childrens
+                n[i].children = childrens
 
-            if isinstance(n, list):
-                for i in range(N):
-                    self.create_tree(r, n[i].children, idx+1)
-            else:
-                self.create_tree(r, n.children, idx+1)
+            for i in range(N):
+                if n[i].children:
+                    self.create_tree(r, n[i].children)
+                else:
+                    self.idx += 1
 
     def print_tree(self, node):
         if not node:
             return
         print("==print==")
-        if isinstance(node, list):
-            for i in range(len(node)):
-                print(node[i].val)
-        else:
-            print(node.val)
+        for i in range(len(node)):
+            print(node[i].val)
 
-        if isinstance(node, list):
-            for i in range(len(node)):
-                self.print_tree(node[i].children)
-        else:
-            self.print_tree(node.children)
-    """
+        for i in range(len(node)):
+            self.print_tree(node[i].children)
 
-    def get_depth(self, node):
-        if not node:
-            return 0
-
-        ans = 0
-        q = deque([[node, 1]])
-        while q:
-            n, d = q.popleft()
-            if n:
-                ans = max(ans, d)
-            if isinstance(n, list):
-                for i in range(len(n)):
-                    if n[i].children:
-                        q.append([n[i].children, d+1])
-            else:
-                q.append([n.children, d+1])
-        return ans
+    def clear(self):
+        self.idx = 2
 
     def maxDepth(self, root):
         """
         :type root: Node
         :rtype: int
         """
-        head = Node(root[0], [])
-        self.create_tree(root, head, 2)
-        self.print_tree(head)
+        def get_depth(node):
+            # 0.30547391399999996
+            # ans = 0
+            # q = deque([[node, 1]])
+            # while q:
+            #     n, d = q.popleft()
+            #     if n:
+            #         ans = max(ans, d)
+            #     for i in range(len(n)):
+            #         if n[i].children:
+            #             q.append([n[i].children, d + 1])
+            # return ans
 
-        return self.get_depth(head)
+            # code refactoring - 0.28522715099999996
+            if not node:
+                return 1
+            dep = 0
+            for i in range(len(node)):
+                if node[i].children:
+                    dep = max(dep, get_depth(node[i].children)+1)
+            return dep
+
+        head = [Node(root[0], [])]
+        self.create_tree(root, head)
+        # self.print_tree(head)
+        if not head:
+            return 0
+        return get_depth(head)+1
+
 
 s = Solution()
-# print(s.maxDepth([1,None,3,2,4,None,5,6])) # 3
-print(s.maxDepth([1,None,2,3,4,5,None,None,6,7,None,8,None,9,10,None,None,11,None,12,None,13,None,None,14]))
+print(s.maxDepth([1,None,3,2,4,None,5,6])) # 3
+s.clear()
+print(s.maxDepth([1,None,2,3,4,5,None,None,6,7,None,8,None,9,10,None,None,11,None,12,None,13,None,None,14])) # 5
 
-"""
-0 (1) 2 3 4 (5) 6 7 8 9 10 11 (6)
-"""
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [[1,None,3,2,4,None,5,6],
+             [1,None,2,3,4,5,None,None,6,7,None,8,None,9,10,None,None,11,None,12,None,13,None,None,14]]
+    t = Timer(f"for t in {query}: Solution().maxDepth(t)", "from __main__ import Solution")
+    print(t.timeit(number=10000))
