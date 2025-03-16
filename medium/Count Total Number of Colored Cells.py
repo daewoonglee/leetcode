4e511f90e778bd1377520dbe67dd1264b375962a
dw.lee <dw.lee832@gmail.com>
2025-03-17 07:04:25 +0900

    Count Total Number of Colored Cells 문제풀이 01 - 풀이O

---
+++ medium/Count Total Number of Colored Cells.py
@@ -0,0 +1,21 @@
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        # dp version
+        # dp = [1]*(n+1)
+        # for i in range(2, n+1):
+        #     dp[i] = dp[i-1] + 4*(i-1)
+        # return dp[-1]
+
+        ans = 1
+        for i in range(2, n+1):
+            ans += 4*(i-1)
+        return ans
+
+
+s = Solution()
+print(s.coloredCells(1)) # 1    +0  4*(n-1)
+print(s.coloredCells(2)) # 5    +4
+print(s.coloredCells(3)) # 13   +8
+print(s.coloredCells(4)) # 25   +12
+print(s.coloredCells(5)) # 41   +16
+print(s.coloredCells(6)) # 61   +20
