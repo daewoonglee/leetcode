b706a6c106b4d50de9eb9eaa9f41e27b5cec9857
dw.lee <dw.lee832@gmail.com>
2025-03-17 07:05:01 +0900

    Count Total Number of Colored Cells 문제풀이 01 - code refactoring

--- medium/Count Total Number of Colored Cells.py
+++ medium/Count Total Number of Colored Cells.py
@@ -6,10 +6,13 @@ class Solution:
         #     dp[i] = dp[i-1] + 4*(i-1)
         # return dp[-1]
 
-        ans = 1
-        for i in range(2, n+1):
-            ans += 4*(i-1)
-        return ans
+        # ans = 1
+        # for i in range(2, n+1):
+        #     ans += 4*(i-1)
+        # return ans
+
+        # code refactoring
+        return n*n*2 - 2*n+1
 
 
 s = Solution()
