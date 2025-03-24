83d72bbcef32cd6e8b6429a9fbae68a5b4170dfd
dw.lee <dw.lee832@gmail.com>
2025-03-24 21:36:20 +0900

    Minimu Index Sum of Two Lists.py 문제풀이 01 - code refactoring

--- easy/Minimum Index Sum of Two Lists.py
+++ easy/Minimum Index Sum of Two Lists.py
@@ -1,15 +1,24 @@
 class Solution:
     def findRestaurant(self, list1: list[str], list2: list[str]) -> list[str]:
-        ans = []
+        li1_dict = {k: i for i, k in enumerate(list1)}
+        li2_dict = {k: i for i, k in enumerate(list2)}
+        list1.sort()
+        list2.sort()
         min_idx = float('inf')
-        for i, li1 in enumerate(list1):
-            for j, li2 in enumerate(list2):
-                if li1 == li2:
-                    if i+j < min_idx:
-                        ans = [li1]
-                        min_idx = i+j
-                    elif i+j == min_idx:
-                        ans.append(li1)
+        i, j = 0, 0
+        ans = []
+        while i < len(list1) and j < len(list2):
+            if list1[i] == list2[j]:
+                if li1_dict[list1[i]] + li2_dict[list2[j]] < min_idx:
+                    ans = [list1[i]]
+                    min_idx = li1_dict[list1[i]] + li2_dict[list2[j]]
+                elif li1_dict[list1[i]] + li2_dict[list2[j]] == min_idx:
+                    ans.append(list1[i])
+                j += 1
+            elif list1[i] < list2[j]:
+                i += 1
+            else:
+                j += 1
         return ans
 
 
