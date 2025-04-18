f3261ae909ea3dd047fa2e82ea6f80e6a00c2c7f
dw.lee <dw.lee832@gmail.com>
2025-04-18 23:19:33 +0900

    Robot Return to Origin 문제풀이 01 - code refactoring

--- easy/Robot Return to Origin.py
+++ easy/Robot Return to Origin.py
@@ -1,17 +1,23 @@
+from collections import Counter
+
+
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        UD = 0
-        LR = 0
-        for m in moves:
-            if m == "R":
-                LR += 1
-            elif m == "L":
-                LR -= 1
-            elif m == "U":
-                UD += 1
-            else:
-                UD -= 1
-        return True if UD == 0 and LR == 0 else False
+        # UD = 0
+        # LR = 0
+        # for m in moves:
+        #     if m == "R":
+        #         LR += 1
+        #     elif m == "L":
+        #         LR -= 1
+        #     elif m == "U":
+        #         UD += 1
+        #     else:
+        #         UD -= 1
+        # return True if UD == 0 and LR == 0 else False
+
+        c = Counter(moves)
+        return True if c["U"] - c["D"] == 0 and c["L"] - c["R"] == 0 else False
 
 
 s = Solution()
