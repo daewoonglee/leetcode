cc1d99c80f222aac67030c6d35ba81d340b73311
dw.lee <dw.lee832@gmail.com>
2025-04-18 23:19:00 +0900

    Robot Return to Origin 문제풀이 01 - 풀이 O

---
+++ easy/Robot Return to Origin.py
@@ -0,0 +1,21 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        UD = 0
+        LR = 0
+        for m in moves:
+            if m == "R":
+                LR += 1
+            elif m == "L":
+                LR -= 1
+            elif m == "U":
+                UD += 1
+            else:
+                UD -= 1
+        return True if UD == 0 and LR == 0 else False
+
+
+s = Solution()
+print(s.judgeCircle("UD")) # true
+print(s.judgeCircle("LL")) # false
+print(s.judgeCircle("L")) # false
+print(s.judgeCircle("UUUUUDDDDDRD")) # false
