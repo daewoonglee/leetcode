49c9511af2b925f43acf504815c65a62e44f2e56
dw.lee <dw.lee832@gmail.com>
2025-04-22 21:51:52 +0900

    Degree of an Array 문제풀이 01 - code refactoring

--- easy/Degree of an Array.py
+++ easy/Degree of an Array.py
@@ -4,24 +4,19 @@ from collections import Counter
 class Solution:
     def findShortestSubArray(self, nums: list[int]) -> int:
         N = len(nums)
+        subarray = {n: [-1, N] for n in nums}
+        for i, n in enumerate(nums):
+            idx = 0 if subarray[n][0] == -1 else 1
+            subarray[n][idx] = i
+
+        ans = N
         count_dict = sorted(Counter(nums).items(), key=lambda x: x[1])[::-1]
         max_count = count_dict[0][1]
-        max_dict = {}
+        if max_count == 1: return 1
         for k, v in count_dict:
             if v != max_count:
                 break
-            max_dict[k] = [0, N-1]
-
-        ans = N
-        for k, (start_idx, end_idx) in max_dict.items():
-            while start_idx < end_idx:
-                if nums[start_idx] == k:
-                    break
-                start_idx += 1
-            while start_idx < end_idx:
-                if nums[end_idx] == k:
-                    break
-                end_idx -= 1
+            start_idx, end_idx = subarray[k]
             ans = ans if ans < (end_idx - start_idx + 1) else (end_idx - start_idx + 1)
         return ans
 
