775d2e8f642e48645c5b41aeb31ec1f3f40cae2d
dw.lee <dw.lee832@gmail.com>
2025-04-25 14:57:36 +0900

    Sum of All Subset XOR Totals 문제풀이 01 - code refactoring

--- easy/Sum of All Subset XOR Totals.py
+++ easy/Sum of All Subset XOR Totals.py
@@ -1,19 +1,15 @@
-from itertools import combinations
-
-
 class Solution:
     def subsetXORSum(self, nums: list[int]) -> int:
-        ans = sum(nums)
-        for r in range(2, len(nums)+1):
-            for c in combinations(nums, r):
-                a = c[0]
-                for i in range(1, len(c)):
-                    a = a ^ c[i]
-                ans += a
-        return ans
+        def dfs(idx, xor):
+            if idx == len(nums):
+                return xor
+            with_cur = dfs(idx+1, xor ^ nums[idx])
+            without_cur = dfs(idx+1, xor)
+            return with_cur + without_cur
+        return dfs(0, 0)
 
 
 s = Solution()
 # print(s.subsetXORSum([1,3])) # 6
-# print(s.subsetXORSum([5,1,6])) # 28
-print(s.subsetXORSum([3,4,5,6,7,8])) # 480
+print(s.subsetXORSum([5,1,6])) # 28
+# print(s.subsetXORSum([3,4,5,6,7,8])) # 480
