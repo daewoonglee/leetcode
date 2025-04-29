5e064fd1f878bebcbba3bdd66aba320fdf0bb7f0
dw.lee <dw.lee832@gmail.com>
2025-04-29 21:09:19 +0900

    Convert the Temperature 문제풀이 01 - code refactoring

--- easy/Convert the Temperature.py
+++ easy/Convert the Temperature.py
@@ -1,13 +1,7 @@
-from decimal import Decimal, getcontext
-
-
 class Solution:
     def convertTemperature(self, celsius: float) -> list[float]:
-        getcontext().prec = 11
-        celsius = Decimal(celsius)
-        kelvin = celsius + Decimal('273.15')
-        fahrenheit = celsius * Decimal(1.8) + Decimal('32')
-        return [kelvin.quantize(Decimal('0.00001')), fahrenheit.quantize(Decimal('0.00001'))]
+        # 파이썬에선 5자리 맞추지 않고 반환해도 답을 인정
+        return [celsius + 273.15, (celsius * 1.8) + 32]
 
 
 s = Solution()
