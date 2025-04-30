287518829148b10952a2f6d131ae249d85f5221f
dw.lee <dw.lee832@gmail.com>
2025-04-30 22:50:06 +0900

    Longest Continuous Increasing Subsequence 문제풀이 01 - 풀이 O

---
+++ easy/Longest Continuous Increasing Subsequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findLengthOfLCIS(self, nums: list[int]) -> int:
+        ans = 1
+        temp = 1
+        for i in range(1, len(nums)):
+            if nums[i-1] < nums[i]:
+                temp += 1
+            else:
+                ans = ans if ans > temp else temp
+                temp = 1
+        return ans if ans > temp else temp
+
+
+s = Solution()
+# print(s.findLengthOfLCIS([1,3,5,4,7])) # 3
+# print(s.findLengthOfLCIS([2,2,2,2,2])) # 1
+# print(s.findLengthOfLCIS([2])) # 1
+# print(s.findLengthOfLCIS([5,4,3,2,1])) # 1
+print(s.findLengthOfLCIS([-3,-2,-1,0,-1,-2])) # 4
