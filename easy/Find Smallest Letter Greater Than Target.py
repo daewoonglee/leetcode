07d9bb96af712a26cdd567fd73602ea064bdace4
dw.lee <dw.lee832@gmail.com>
2025-06-03 23:21:56 +0900

    Find Smallest Letter Greater Than Target 문제풀이 01 - code refactoring

--- easy/Find Smallest Letter Greater Than Target.py
+++ easy/Find Smallest Letter Greater Than Target.py
@@ -1,13 +1,20 @@
 class Solution:
     def nextGreatestLetter(self, letters: list[str], target: str) -> str:
         letters.sort()
-        for ch in letters:
-            if ch > target:
-                return ch
-        return letters[0]
+        if letters[-1] <= target: return letters[0]
+
+        l, r = 0, len(letters)-1
+        while l <= r:
+            p = l + (r-l)//2
+            if letters[p] <= target:
+                l = p + 1
+            else:
+                r = p - 1
+        return letters[l]
 
 
 s = Solution()
 print(s.nextGreatestLetter(["c","f","j"], "a")) # c
 print(s.nextGreatestLetter(["c","f","j"], "c")) # f
 print(s.nextGreatestLetter(["x","x","y","y"], "z")) # x
+print(s.nextGreatestLetter(["x","y","z"], "z")) # x
