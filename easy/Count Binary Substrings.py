fd17edd563c5d9e5aa21b30a7293dac3e89d2bbd
dw.lee <dw.lee832@gmail.com>
2026-03-04 08:25:44 +0900

    Count Binary Substrings 문제풀이 02 - 풀이O

--- easy/Count Binary Substrings.py
+++ easy/Count Binary Substrings.py
@@ -1,27 +1,19 @@
 class Solution:
     def countBinarySubstrings(self, s: str) -> int:
-        def search_group(i):
-            if i >= N:
-                return i
+        log = []
+        target, cnt = s[0], 1
+        for ch in s[1:]:
+            if target == ch:
+                cnt += 1
+            else:
+                log.append(cnt)
+                target = ch
+                cnt = 1
+        log.append(cnt)
 
-            target = s[i]
-            for j in range(i+1,  N, 1):
-                if target == s[j]:
-                    i += 1
-                else:
-                    break
-            return i
-
-        N = len(s)
-        cur_idx, tail_idx = 0, N
         ans = 0
-        while 1:
-            cur_idx = search_group(cur_idx)
-            tail_idx = search_group(cur_idx+1)
-            print(f"cur: {cur_idx}, tail: {tail_idx}")
-            if tail_idx == N: break
-            ans += tail_idx - cur_idx
-            cur_idx += 1
+        for i in range(len(log)-1):
+            ans += min(log[i], log[i+1])
         return ans
 
 
@@ -31,4 +23,5 @@ s = Solution()
 # print(s.countBinarySubstrings("10101"))     # 4
 # print(s.countBinarySubstrings("1"))         # 0
 # print(s.countBinarySubstrings("111"))       # 0
-print(s.countBinarySubstrings("00100"))     # 2
+# print(s.countBinarySubstrings("00100"))     # 2
+print(s.countBinarySubstrings("0010001"))   # 3
