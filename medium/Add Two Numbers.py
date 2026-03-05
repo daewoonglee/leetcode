03ad5bef5dca5050406505a5cc545030534958aa
dw.lee <dw.lee832@gmail.com>
2026-03-05 09:21:54 +0900

    Add Two Numbers 문제풀이 01 - code refactoring

--- medium/Add Two Numbers.py
+++ medium/Add Two Numbers.py
@@ -10,25 +10,27 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        num_li1, num_li2 = [], []
+        head, cur = None, None
+        q = 0
         while l1 or l2:
+            n = q
             if l1 is not None:
-                num_li1.append(l1.val)
+                n += l1.val
                 l1 = l1.next
             if l2 is not None:
-                num_li2.append(l2.val)
+                n += l2.val
                 l2 = l2.next
-        num_li1.reverse()
-        num_li2.reverse()
 
-        head, cur = None, None
-        for n in str(int("".join(map(str, num_li1))) + int("".join(map(str, num_li2))))[::-1]:
+            q, n = divmod(n, 10)
             if head is None:
-                head = ListNode(int(n))
+                head = ListNode(n)
                 cur = head
             else:
-                cur.next = ListNode(int(n))
+                cur.next = ListNode(n)
                 cur = cur.next
+        if q != 0:
+            cur.next = ListNode(q)
+
         return head
 
 
@@ -41,3 +43,18 @@ n2.next = ListNode(6)
 n2.next.next = ListNode(4)
 
 print(Solution().addTwoNumbers(n1, n2))
+
+
+n1_head = ListNode(9)
+cur1 = n1_head
+for i in range(6):
+    cur1.next = ListNode(9)
+    cur1 = cur1.next
+
+n2_head = ListNode(9)
+cur2 = n2_head
+for i in range(3):
+    cur2.next = ListNode(9)
+    cur2 = cur2.next
+
+print(Solution().addTwoNumbers(n1_head, n2_head))
