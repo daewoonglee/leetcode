fe0aaab9559dae912f29199d063ad31317d02871
dw.lee <dw.lee832@gmail.com>
2026-03-06 15:01:40 +0900

    Longest Substring Without Repeating Characters 문제풀이 01 - code refactoring 02

--- medium/Longest Substring Without Repeating Characters.py
+++ medium/Longest Substring Without Repeating Characters.py
@@ -3,13 +3,23 @@ from collections import deque
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        # ans = 0
+        # substr = deque()
+        # for ch in s:
+        #     while ch in substr:
+        #         substr.popleft()
+        #     substr.append(ch)
+        #     ans = ans if ans > len(substr) else len(substr)
+        # return ans
+
+        last = {}
+        cur_idx = -1
         ans = 0
-        substr = deque()
-        for ch in s:
-            while ch in substr:
-                substr.popleft()
-            substr.append(ch)
-            ans = ans if ans > len(substr) else len(substr)
+        for i, ch in enumerate(s):
+            if ch in last:
+                cur_idx = last[ch] if last[ch] > cur_idx else cur_idx
+            last[ch] = i
+            ans = ans if ans > i - cur_idx else i - cur_idx
         return ans
 
 
