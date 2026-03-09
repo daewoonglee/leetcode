faee47dc3c6f9ea211bd71d42d056ccb2acc75b4
dw.lee <dw.lee832@gmail.com>
2026-03-09 09:25:57 +0900

    Find Unique Binary String 문제풀이 01 - code refactoring 02

--- medium/Find Unique Binary String.py
+++ medium/Find Unique Binary String.py
@@ -1,16 +1,26 @@
 class Solution:
     def findDifferentBinaryString(self, nums: list[str]) -> str:
-        N = len(nums)
-        unique_bin = {bin(i)[2:].zfill(N) : 1 for i in range(2**N)}
-        for n in nums:
-            unique_bin[n] = 0
+        # N = len(nums)
+        # nums.sort()
+        # ans = ""
+        # for i in range(2**N):
+        #     ans = bin(i)[2:].zfill(N)
+        #     if ans not in nums:
+        #         break
+        # return ans
 
-        ans = ""
-        for k, v in unique_bin.items():
-            if v:
-                ans = k
-                break
-        return ans
+        # code refactoring 02
+        # N = len(nums)
+        # binary_li = [0 for _ in range(2**N)]
+        # for n in nums:
+        #     binary_li[int(n, 2)] = 1
+        # for i, b in enumerate(binary_li):
+        #     if b == 0:
+        #         return bin(i)[2:].zfill(N)
+        # return ""
+
+        # code refactoring 03 - 문제에선 nums 배열의 길이는 최대 n을 넘지 않음 (이 부분을 제대로 이해 못하고 풀었음)
+        return "".join(['1' if n[i] == '0' else '0' for i, n in enumerate(nums)])
 
 
 s = Solution()
