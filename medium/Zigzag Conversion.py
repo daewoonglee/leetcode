9d5a7efbf73320fc20c7512beb2d177aab3301b9
dw.lee <dw.lee832@gmail.com>
2026-03-25 14:24:45 +0900

    Zigzag Conversion 문제풀이 01 - code refactoring 02

--- medium/Zigzag Conversion.py
+++ medium/Zigzag Conversion.py
@@ -3,24 +3,29 @@ class Solution:
         if numRows == 1:
             return s
 
-        conv = [""] * numRows
-        row, step = 0, 1
-        for ch in s:
-            if row <= 0:
-                step = 1
-            elif row == numRows - 1:
-                step = -1
+        ans = []
+        step = (numRows-1)*2
+        N = len(s)
 
-            conv[row] += ch
-            row += step
-        return "".join(conv)
+        for row in range(numRows):
+            i = row
+            gap1 = step - row * 2
+            gap2 = row * 2
+            while i < N:
+                ans.append(s[i])
+                i += gap1 or step
+                if gap2 and i < N:
+                    ans.append(s[i])
+                    i += gap2
+
+        return "".join(ans)
 
 
 s = Solution()
 # print(s.convert("A", 1)) # A
 # print(s.convert("AB", 1)) # A
 # print(s.convert("AB", 2)) # AB
-# print(s.convert("PAYPALISHIRING", 2)) # PYAIHRNAPLSIIG
+print(s.convert("PAYPALISHIRING", 2)) # PYAIHRNAPLSIIG
 # print(s.convert("PAYPALISHIRING", 3)) # PAHNAPLSIIGYIR
 # print(s.convert("PAYPALISHIRING", 4)) # PINALSIGYAHRPI
-print(s.convert("PAYP,ALISHIR.pING", 5)) # PSGAIHNYLIIPARp,.
+# print(s.convert("PAYP,ALISHIR.pING", 5)) # PSGAIHNYLIIPARp,.
