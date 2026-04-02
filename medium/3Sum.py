21b60f36a8b58fbc423069437d13a6e1561dd50a
dw.lee <dw.lee832@gmail.com>
2026-04-02 15:53:24 +0900

    3Sum 문제풀이 03 - code refactoring

--- medium/3Sum.py
+++ medium/3Sum.py
@@ -1,18 +1,26 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
+        N = len(nums)
         nums.sort()
-        ans = []
+        ans = set()
+        is_searched = {}
         for i, target in enumerate(nums):
-            dp = dict()
-            for j in range(i+1, len(nums)):
+            if target > 0: break
+
+            if target in is_searched: continue
+            is_searched[target] = True
+
+            dp = {}
+            for j in range(i+1, N):
                 n = -(nums[j] + target)
                 if n in dp:
-                    ans.append((target, nums[j], n))
+                    ans.add((target, nums[j], n))
                 dp[nums[j]] = n
-        return [list(t) for t in set(ans)]
+        return [list(t) for t in ans]
 
 
 s = Solution()
-print(s.threeSum([-1,0,1,2,-1,-4])) # [[-1,-1,2],[-1,0,1]]
+# print(s.threeSum([-1,0,1,2,-1,-4])) # [[-1,-1,2],[-1,0,1]]
 # print(s.threeSum([0,1,1])) # []
 # print(s.threeSum([0,0,0])) # [[0,0,0]]
+print(s.threeSum([-1,0,1,2,-1,-4,-2,-3,7])) # [[-1,-1,2],[-1,0,1]]
