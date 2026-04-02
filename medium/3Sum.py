1cbfc005149e2bda01032af8605889b99e0d1ed1
dw.lee <dw.lee832@gmail.com>
2026-04-02 16:49:42 +0900

    3Sum 문제풀이 03 - code refactoring 02

--- medium/3Sum.py
+++ medium/3Sum.py
@@ -2,25 +2,32 @@ class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         N = len(nums)
         nums.sort()
-        ans = set()
-        is_searched = {}
+        ans = []
         for i, target in enumerate(nums):
             if target > 0: break
+            if i > 0 and target == nums[i - 1]: continue
 
-            if target in is_searched: continue
-            is_searched[target] = True
-
-            dp = {}
-            for j in range(i+1, N):
-                n = -(nums[j] + target)
-                if n in dp:
-                    ans.add((target, nums[j], n))
-                dp[nums[j]] = n
-        return [list(t) for t in ans]
+            L, R = i + 1, N - 1
+            while L < R:
+                total = target + nums[L] + nums[R]
+                if total == 0:
+                    ans.append([target, nums[L], nums[R]])
+                    L += 1
+                    R -= 1
+                    while L < R and nums[L] == nums[L - 1]:
+                        L += 1
+                elif total > 0:
+                    R -= 1
+                else:
+                    L += 1
+        return ans
 
 
 s = Solution()
 # print(s.threeSum([-1,0,1,2,-1,-4])) # [[-1,-1,2],[-1,0,1]]
 # print(s.threeSum([0,1,1])) # []
 # print(s.threeSum([0,0,0])) # [[0,0,0]]
-print(s.threeSum([-1,0,1,2,-1,-4,-2,-3,7])) # [[-1,-1,2],[-1,0,1]]
+# print(s.threeSum([-1,0,1,2,-1,-4,-2,-3,7])) # [[-4, -3, 7], [-3, 1, 2], [-2, 0, 2], [-1, -1, 2], [-1, 0, 1]]
+# print(s.threeSum([-4,-3,-2,-1,-1,0,1,2,7])) # [[-4, -3, 7], [-3, 1, 2], [-2, 0, 2], [-1, -1, 2], [-1, 0, 1]]
+# print(s.threeSum([-4,-3,0,2,2,2,2,7])) # [[-4, -3, 7], [-4, 2, 2]]
+print(s.threeSum([-1,0,0,0,0,1,1,1,1])) # [[-1, 0, 1], [0, 0, 0]]
