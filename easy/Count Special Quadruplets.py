ee85f49a17b56fd8504c90e87aff878826d17907
dw.lee <dw.lee832@gmail.com>
2026-04-07 14:49:14 +0900

    Count Special Quadruplets 문제풀이 01 - code refactoring 02

--- easy/Count Special Quadruplets.py
+++ easy/Count Special Quadruplets.py
@@ -1,21 +1,17 @@
-from collections import Counter
+from collections import defaultdict
 
 
 class Solution:
     def countQuadruplets(self, nums: list[int]) -> int:
         N = len(nums)
         ans = 0
-        log = Counter()
-        log[nums[-1]] = 1
-        for k in range(N-2, 1, -1):
-            for i in range(k-1):
-                for j in range(i+1, k):
-                    total = nums[i] + nums[j] + nums[k]
-                    if total in log:
-                        ans += log[total]
-            log[nums[k]] += 1
+        log = defaultdict(int)
+        for k in range(1, N):
+            for i in range(k+1, N): # a+b = d-c
+                ans += log[nums[i] - nums[k]]
+            for i in range(k):
+                log[nums[i] + nums[k]] += 1
         return ans
-
 
 
 s = Solution()
@@ -24,17 +20,3 @@ s = Solution()
 # print(s.countQuadruplets([1,1,1,3,5])) # 4
 # print(s.countQuadruplets([1,1,3,1,5])) # 3
 print(s.countQuadruplets([1,1,3,1,5,5])) # 6
-'''
-[1,1,1,3,5]
-0 1 3 4 -> 1 1 3 5
-0 2 3 4 -> 1 1 3 5
-1 2 3 4 -> 1 1 3 5
-0 1 2 3 -> 1 1 1 3
-
-
-[1,1,3,1,5]
-0 1 2 4 -> 1 1 3 5
-0 2 3 4 -> 1 3 1 5
-1 2 3 4 -> 1 3 1 5
-'''
-# print(s.countQuadruplets([1,1,1,1,3])) # 4
