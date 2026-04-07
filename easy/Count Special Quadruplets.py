996360d6e0319a97714cd205903b81ad1862f231
dw.lee <dw.lee832@gmail.com>
2026-04-07 14:18:05 +0900

    Count Special Quadruplets 문제풀이 01 - code refactoring

--- easy/Count Special Quadruplets.py
+++ easy/Count Special Quadruplets.py
@@ -1,16 +1,21 @@
+from collections import Counter
+
+
 class Solution:
     def countQuadruplets(self, nums: list[int]) -> int:
-        # nums.sort()
         N = len(nums)
         ans = 0
-        for i in range(N-3):
-            for j in range(i+1, N-2):
-                for z in range(j+1, N-1):
-                    for k in range(z+1, N):
-                        if nums[i] + nums[j] + nums[z] == nums[k]:
-                            ans += 1
-                            print(i, j, z, k)
+        log = Counter()
+        log[nums[-1]] = 1
+        for k in range(N-2, 1, -1):
+            for i in range(k-1):
+                for j in range(i+1, k):
+                    total = nums[i] + nums[j] + nums[k]
+                    if total in log:
+                        ans += log[total]
+            log[nums[k]] += 1
         return ans
+
 
 
 s = Solution()
@@ -18,4 +23,18 @@ s = Solution()
 # print(s.countQuadruplets([3,3,6,4,5])) # 0
 # print(s.countQuadruplets([1,1,1,3,5])) # 4
 # print(s.countQuadruplets([1,1,3,1,5])) # 3
-print(s.countQuadruplets([1,1,1,1,3])) # 4
+print(s.countQuadruplets([1,1,3,1,5,5])) # 6
+'''
+[1,1,1,3,5]
+0 1 3 4 -> 1 1 3 5
+0 2 3 4 -> 1 1 3 5
+1 2 3 4 -> 1 1 3 5
+0 1 2 3 -> 1 1 1 3
+
+
+[1,1,3,1,5]
+0 1 2 4 -> 1 1 3 5
+0 2 3 4 -> 1 3 1 5
+1 2 3 4 -> 1 3 1 5
+'''
+# print(s.countQuadruplets([1,1,1,1,3])) # 4
