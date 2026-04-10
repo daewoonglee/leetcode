fb1af942d2ad23a69f0311b616c34680f017fbe7
dw.lee <dw.lee832@gmail.com>
2026-04-10 15:14:12 +0900

    Count Good Triplets 문제풀이 01 - code refactoring 02

--- easy/Count Good Triplets.py
+++ easy/Count Good Triplets.py
@@ -1,17 +1,21 @@
+from bisect import bisect_left
+
+
 class Solution:
     def countGoodTriplets(self, arr: list[int], a: int, b: int, c: int) -> int:
         N = len(arr)
         ans = 0
-        for i in range(N-2):
-            for j in range(i+1, N-1):
-                if abs(arr[i] - arr[j]) > a: continue
-                for k in range(j+1, N):
-                    if abs(arr[j]-arr[k]) <= b and abs(arr[i]-arr[k]) <= c:
-                        ans += 1
+        for j in range(1, N-1):
+            left = [arr[i] for i in range(j) if abs(arr[i] - arr[j]) <= a]
+            right = [arr[k] for k in range(j+1, N) if abs(arr[j] - arr[k]) <= b]
+            right.sort()
+
+            for l in left:
+                ans += bisect_left(right, l+c+1) - bisect_left(right, l-c)
         return ans
 
 
 s = Solution()
 # print(s.countGoodTriplets([3,0,1,1,9,7], a=7, b=2, c=3)) # 4
 # print(s.countGoodTriplets([1,1,2,2,3], a=0, b=0, c=1)) # 0
-print(s.countGoodTriplets([0,0,0,0,0], a=0, b=0, c=0)) # 0
+print(s.countGoodTriplets([0,0,0,0,0], a=0, b=0, c=0)) # 10
